ca17412db22a37a8d67a02f20803790f328ea3ca
govert_cp <SND\govert_cp>
2009-02-09 03:52:22 -0800

    ExcelDna Version 0.6 2006/11/15

--- Source/AddIn/AssemblyInfo.cpp
+++ Source/AddIn/AssemblyInfo.cpp
@@ -51,7 +51,7 @@ using namespace System::Runtime::InteropServices;
 // You can specify all the value or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersionAttribute("0.1.*")];
+[assembly:AssemblyVersionAttribute("0.6.*")];
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
