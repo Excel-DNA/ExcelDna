0bcf0d38ce82033615700cf0f779fa8f275b4d49
govert_cp <SND\govert_cp>
2014-05-29 12:44:15 -0700

    * Suppress CustomUI loading error when Excel is running embedded.

--- Source/ExcelDna.Loader/Properties/AssemblyInfo.cs
+++ Source/ExcelDna.Loader/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.33.*")]
-[assembly: AssemblyFileVersion("0.33.1.1")]
+[assembly: AssemblyFileVersion("0.33.2.1")]
