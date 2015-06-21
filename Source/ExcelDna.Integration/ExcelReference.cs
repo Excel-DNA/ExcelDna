0503231da50f3ab537047f16ba5b6985d70cbc79
Govert van Drimmelen <govert@icon.co.za>
2015-06-21 23:31:27 +0200

    Clean up ExcelReference implementation a bit

--- Source/ExcelDna.Integration/ExcelReference.cs
+++ Source/ExcelDna.Integration/ExcelReference.cs
@@ -31,7 +31,7 @@ namespace ExcelDna.Integration
     // CAUTION: The ExcelReference class is also called via reflection by the ExcelDna.Loader marshaler.
 	public class ExcelReference : IEquatable<ExcelReference>
 	{
-	    class ExcelRectangle : IEquatable<ExcelRectangle>
+	    struct ExcelRectangle : IEquatable<ExcelRectangle>
         {
             public readonly int RowFirst;
             public readonly int RowLast;
@@ -41,17 +41,17 @@ namespace ExcelDna.Integration
             internal ExcelRectangle(int rowFirst, int rowLast, int columnFirst, int columnLast)
             {
                 // CONSIDER: Throw or truncate for errors
-                RowFirst    = GetInRange(rowFirst, 0, ExcelDnaUtil.ExcelLimits.MaxRows - 1);
-                RowLast     = GetInRange(rowLast, 0, ExcelDnaUtil.ExcelLimits.MaxRows - 1);
-                ColumnFirst = GetInRange(columnFirst, 0, ExcelDnaUtil.ExcelLimits.MaxColumns - 1);
-                ColumnLast  = GetInRange(columnLast, 0, ExcelDnaUtil.ExcelLimits.MaxColumns - 1);
+                RowFirst    = Clamp(rowFirst, 0, ExcelDnaUtil.ExcelLimits.MaxRows - 1);
+                RowLast     = Clamp(rowLast, 0, ExcelDnaUtil.ExcelLimits.MaxRows - 1);
+                ColumnFirst = Clamp(columnFirst, 0, ExcelDnaUtil.ExcelLimits.MaxColumns - 1);
+                ColumnLast  = Clamp(columnLast, 0, ExcelDnaUtil.ExcelLimits.MaxColumns - 1);
                 
                 // CONSIDER: Swap or truncate rect ??
                 //if (RowLast < RowFirst) RowLast = RowFirst;
                 //if (ColumnLast < ColumnFirst) ColumnLast = RowFirst;
             }
 
-            private int GetInRange(int value, int min, int max)
+            static int Clamp(int value, int min, int max)
             {
                 Debug.Assert(min <= max);
                 if (value < min) return min;
@@ -61,16 +61,12 @@ namespace ExcelDna.Integration
 
             public override bool Equals(object obj)
             {
-                if (ReferenceEquals(null, obj)) return false;
-                if (ReferenceEquals(this, obj)) return true;
                 if (obj.GetType() != typeof (ExcelRectangle)) return false;
                 return Equals((ExcelRectangle) obj);
             }
 
             public bool Equals(ExcelRectangle other)
             {
-                if (ReferenceEquals(null, other)) return false;
-                if (ReferenceEquals(this, other)) return true;
                 return other.RowFirst == RowFirst && other.RowLast == RowLast && other.ColumnFirst == ColumnFirst && other.ColumnLast == ColumnLast;
             }
 
@@ -87,10 +83,10 @@ namespace ExcelDna.Integration
             }
         }
 
-        // CONSIDER: Rather use a derived class so that we can implement Equals properly.
-        //           The implementation as a List here is actually hidden.
-	    readonly List<ExcelRectangle> rectangles = new List<ExcelRectangle>();
 		readonly IntPtr sheetId;
+        // Save first rectangle explicitly, to avoid array if we don't need one.
+        readonly ExcelRectangle rectangle;
+	    ExcelRectangle[] furtherRectangles; // Can't make readonly (yet) since we still have the obsolete 'AddReference'
 
 		public ExcelReference(int row, int column)
 			: this(row, row, column, column)
@@ -115,8 +111,7 @@ namespace ExcelDna.Integration
 		public ExcelReference(int rowFirst, int rowLast, int columnFirst, int columnLast, IntPtr sheetId)
 		{
 			this.sheetId = sheetId;
-			ExcelRectangle rect = new ExcelRectangle(rowFirst, rowLast, columnFirst, columnLast);
-			rectangles.Add(rect);
+			rectangle = new ExcelRectangle(rowFirst, rowLast, columnFirst, columnLast);
 		}
 
         // TODO: Consider how to deal with invalid sheetName. I presume xlSheetId will fail.
@@ -125,53 +120,68 @@ namespace ExcelDna.Integration
         {
             ExcelReference sheetRef = (ExcelReference)XlCall.Excel(XlCall.xlSheetId, sheetName);
             this.sheetId = sheetRef.SheetId;
-            ExcelRectangle rect = new ExcelRectangle(rowFirst, rowLast, columnFirst, columnLast);
-            rectangles.Add(rect);
+            rectangle = new ExcelRectangle(rowFirst, rowLast, columnFirst, columnLast);
         }
 
         public ExcelReference(int[][] rectangles, IntPtr sheetId)
         {
             this.sheetId = sheetId;
             int rectCount = rectangles.Length;
-            for (int i = 0; i < rectCount; i++)
+            int[] rect = rectangles[0];
+            rectangle = new ExcelRectangle(rect[0], rect[1], rect[2], rect[3]);
+
+            int furtherRectangleCount = rectCount - 1;
+            if (furtherRectangleCount > 0)
             {
-                int[] rect = rectangles[i];
-                Debug.Assert(rect.Length == 4);
-                this.rectangles.Add(new ExcelRectangle(rect[0], rect[1], rect[2], rect[3]));
+                furtherRectangles = new ExcelRectangle[furtherRectangleCount];
+                for (int i = 0; i < furtherRectangleCount; i++)
+                {
+                    rect = rectangles[i + 1];
+                    Debug.Assert(rect.Length == 4);
+                    furtherRectangles[i] = new ExcelRectangle(rect[0], rect[1], rect[2], rect[3]);
+                }
             }
         }
 
 		// THROWS: OverFlowException if the arguments exceed the allowed size
 		// or if the number of Inner References exceeds 65000
-        // NOTE: Currently used (via reflection) from ExcelDna.Loader to populate the rectangles. 
-        // TODO: Change to use a safer mechanism
         [Obsolete("An ExcelReference should never be modified.")]
 		public void AddReference(int rowFirst, int rowLast, int columnFirst, int columnLast)
 		{
-			if (rectangles.Count < ushort.MaxValue)
-				rectangles.Add(new ExcelRectangle(rowFirst, rowLast, columnFirst, columnLast));
-			else 
+            ExcelRectangle rect = new ExcelRectangle(rowFirst, rowLast, columnFirst, columnLast);
+            if (furtherRectangles == null)
+            {
+                furtherRectangles = new ExcelRectangle[1];
+                furtherRectangles[0] = rect;
+                return;
+            }
+			if (furtherRectangles.Length >= ushort.MaxValue - 1)
 				throw new OverflowException("Maximum number of references exceeded");
+
+            ExcelRectangle[] newRectangles = new ExcelRectangle[furtherRectangles.Length + 1];
+            Array.Copy(furtherRectangles, newRectangles, furtherRectangles.Length);
+            newRectangles[newRectangles.Length - 1] = rect;
+            furtherRectangles = newRectangles;
 		}
 
 		public int RowFirst
 		{
-			get { return rectangles[0].RowFirst; }
+			get { return rectangle.RowFirst; }
 		}
 
 		public int RowLast
 		{
-			get { return rectangles[0].RowLast; }
+			get { return rectangle.RowLast; }
 		}
 
 		public int ColumnFirst
 		{
-			get { return rectangles[0].ColumnFirst; }
+			get { return rectangle.ColumnFirst; }
 		}
 
 		public int ColumnLast
 		{
-			get { return rectangles[0].ColumnLast; }
+			get { return rectangle.ColumnLast; }
 		}
 
 		public IntPtr SheetId
@@ -185,11 +195,16 @@ namespace ExcelDna.Integration
 			get 
 			{
 				List<ExcelReference> inner = new List<ExcelReference>();
-				foreach (ExcelRectangle rect in rectangles)
-				{
-					inner.Add(new ExcelReference(rect.RowFirst, rect.RowLast, 
-						rect.ColumnFirst, rect.ColumnLast, sheetId));
-				}
+                // Add ourselves - which is the first one
+                inner.Add(new ExcelReference(RowFirst, RowLast, ColumnFirst, ColumnLast, SheetId));
+                // And then all the others
+                if (furtherRectangles != null)
+                {
+                    foreach (ExcelRectangle rect in furtherRectangles)
+                    {
+                        inner.Add(new ExcelReference(rect.RowFirst, rect.RowLast, rect.ColumnFirst, rect.ColumnLast, SheetId));
+                    }
+                }
 				return inner;
 			}
 		}
@@ -199,7 +214,7 @@ namespace ExcelDna.Integration
 			return XlCall.Excel(XlCall.xlCoerce, this);
 		}
 
-        // DOCUMENT: Strange behaviour with SetValue...
+        // DOCUMENT: Strange behaviour with SetValue...???
 		public bool SetValue(object value)
 		{
 			return (bool)XlCall.Excel(XlCall.xlSet, this, value);
@@ -209,42 +224,63 @@ namespace ExcelDna.Integration
         // Returns arrays containing all the inner rectangles (including the one we pretend is outside).
         private int[][] GetRectangles()
         {
-            int[][] intRects = new int[rectangles.Count][];
-            for (int i = 0; i < rectangles.Count; i++)
+            int rectangleCount = GetRectangleCount();
+            int furtherRectangleCount = rectangleCount - 1;
+            int[][] intRects = new int[rectangleCount][];
+            intRects[0] = new int[] { RowFirst, RowLast, ColumnFirst, ColumnLast };
+            for (int i = 0; i < furtherRectangleCount; i++)
             {
-                ExcelRectangle rect = rectangles[i];
-                intRects[i] = new int[] {rect.RowFirst, rect.RowLast,
-                    rect.ColumnFirst, rect.ColumnLast};
+                // We only get here if there are further rectangles
+                Debug.Assert(furtherRectangles != null);
+                ExcelRectangle rect = furtherRectangles[i];
+                intRects[i+1] = new int[] {rect.RowFirst, rect.RowLast, rect.ColumnFirst, rect.ColumnLast};
             }
             return intRects;
         }
 
         private int GetRectangleCount()
         {
-            return rectangles.Count;
+            if (furtherRectangles == null)
+                return 1;
+            return furtherRectangles.Length + 1;
         }
 
         // Structural equality implementation
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != typeof (ExcelReference)) return false;
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != typeof (ExcelReference))
+                return false;
             return Equals((ExcelReference) obj);
         }
 
 	    public bool Equals(ExcelReference other)
 	    {
-	        if (ReferenceEquals(null, other)) return false;
-	        if (ReferenceEquals(this, other)) return true;
+	        if (ReferenceEquals(null, other))
+                return false;
+	        if (ReferenceEquals(this, other))
+                return true;
             // Implement equality check based on contents.
-            // CONSIDER: Implement in class derived from List.
-            if (rectangles.Count != other.rectangles.Count) return false;
-            for (int i = 0; i < rectangles.Count; i++)
+            if (!rectangle.Equals(other.rectangle))
+                return false;
+            if (!sheetId.Equals(other.sheetId))
+                return false;
+
+            // Count will implicitly check null furtherRectangle arrays on either side too
+            int rectangleCount = GetRectangleCount();
+            if (rectangleCount != other.GetRectangleCount())
+                return false;
+
+            int furtherRectangleCount = rectangleCount - 1;
+            for (int i = 0; i < furtherRectangleCount; i++)
             {
-                if (!Equals(rectangles[i], other.rectangles[i])) return false; 
+                if (!furtherRectangles[i].Equals(other.furtherRectangles[i]))
+                    return false; 
             }
-	        return other.sheetId.Equals(sheetId);
+	        return true;
 	    }
 
         // We need to take some care with the Hash Code here, since we use the ExcelReference with structural comparison
@@ -258,11 +294,13 @@ namespace ExcelDna.Integration
             
             unchecked
 	        {
-                for (int i = 0; i < rectangles.Count; i++)
+                hash = hash * a + rectangle.GetHashCode();
+                a = a * b;
+                if (furtherRectangles != null)
                 {
-                    if (rectangles[i] != null)
+                    for (int i = 0; i < furtherRectangles.Length; i++)
                     {
-                        hash = hash * a + rectangles[i].GetHashCode();
+                        hash = hash * a + furtherRectangles[i].GetHashCode();
                         a = a * b;
                     }
                 }
