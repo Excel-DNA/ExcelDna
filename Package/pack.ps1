3c82d9cf5fa73436d2845cb3a91028d6b78e576b
Govert van Drimmelen <govert@icon.co.za>
2015-08-30 23:48:57 +0200

    Create pack.ps1

---
+++ Package/pack.ps1
@@ -0,0 +1,5 @@
+Write-Host "Running NuGet packaging script..." 
+$root = $Env:APPVEYOR_BUILD_FOLDER
+$version = "0.33.7-RC2-" + $Env:APPVEYOR_BUILD_VERSION
+nuget pack $root\Package\ExcelDna.Integration\ExcelDna.Integration.nuspec -Version $version
+nuget pack $root\Package\ExcelDna.AddIn\ExcelDna.AddIn.nuspec -Version $version
