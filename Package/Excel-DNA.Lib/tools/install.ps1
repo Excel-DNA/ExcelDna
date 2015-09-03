b3ce89649fb7489228ac5931ad607cfc4f14a602
Govert van Drimmelen <govert@icon.co.za>
2015-09-03 21:31:17 +0200

    Update version to 0.33.8 Rebuild binaries

--- Package/Excel-DNA.Lib/tools/install.ps1
+++
@@ -1,7 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-Write-Host "Starting Excel-DNA.Lib install script"
-
-Write-Host "`tSet reference to ExcelDna.Integration to be CopyLocal=false"
-$project.Object.References | Where-Object { $_.Name -eq 'ExcelDna.Integration' } | ForEach-Object { $_.CopyLocal = $false }
-
-Write-Host "Completed Excel-DNA.Lib install script"
--- Source/ExcelDnaPack/Properties/AssemblyInfo.cs
+++ Source/ExcelDnaPack/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("0.33.*")]
-[assembly: AssemblyFileVersion("0.33.7.1")]
+[assembly: AssemblyFileVersion("0.33.8.1")]
