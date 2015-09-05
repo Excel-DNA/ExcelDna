c787d09d84243eb25d631d58cf5cd77ffe5ff4c1
Caio Proiete <caio.proiete@gmail.com>
2015-09-05 11:08:29 -0300

    Replace post-build events with MSBuild targets + improvements | Resolve #16 and #17

--- Package/ExcelDna.AddIn/tools/install.ps1
+++ Package/ExcelDna.AddIn/tools/install.ps1
@@ -1,138 +1,133 @@
 param($installPath, $toolsPath, $package, $project)
-Write-Host "Starting ExcelDna.AddIn install script"
+    Write-Host "Starting ExcelDna.AddIn install script"
 
-$dteVersion = $project.DTE.Version
-$isBeforeVS2015 = ($dteVersion -lt 14.0)
-$projName = $project.Name
-$isFSharp = ($project.Type -eq "F#")
-# Look for and rename old .dna file
-$newDnaFile = $project.ProjectItems | Where-Object { $_.Name -eq "ExcelDna-Template.dna" }
-$newDnaFileName = "${projName}-AddIn.dna"
-$oldDnaFile = $project.ProjectItems | Where-Object { $_.Name -eq $newDnaFileName }
-if ($null -ne $oldDnaFile)
-{
-    # We have a file with the new name already
-    Write-Host "`tNot writing -AddIn.dna file. File exists already."
-    $newDnaFile.Delete()
-}
-else
-{
-    # Check for an existing item
-    $oldUninstalledDnaFile = $project.ProjectItems | Where-Object { $_.Name -eq "_UNINSTALLED_${newDnaFileName}" }
-    if ($null -ne $oldUninstalledDnaFile)
+    $dteVersion = $project.DTE.Version
+    $isBeforeVS2015 = ($dteVersion -lt 14.0)
+    $isFSharpProject = ($project.Type -eq "F#")
+    $projectName = $project.Name
+
+    # Look for and rename old .dna file
+    $newDnaFile = $project.ProjectItems | Where-Object { $_.Name -eq "ExcelDna-Template.dna" }
+    $newDnaFileName = "${projectName}-AddIn.dna"
+    $oldDnaFile = $project.ProjectItems | Where-Object { $_.Name -eq $newDnaFileName }
+
+    if ($null -ne $oldDnaFile)
     {
-        Write-Host "`tRenaming uninstalled -AddIn.dna file"
-
-        # Write-Host "Found file" + "_UNINSTALLED_${dnaFileName}"
-        $suffix = 1
-        while ($null -ne ($project.ProjectItems | Where-Object { $_.Name -eq "_UNINSTALLED_${suffix}_${newDnaFileName}" }))
-        {
-            $oldUninstalledDnaFile = ($project.ProjectItems | Where-Object { $_.Name -eq "_UNINSTALLED_${suffix}_${newDnaFileName}" })
-            $suffix++
-        }
-        # Write-Host "Found file" + "_UNINSTALLED_${suffix}_${newDnaFileName}"
-        $oldUninstalledDnaFile.Name = $newDnaFileName
-            
-     
-        if ($isFSharp -and $isBeforeVS2015)
-        {
-            # For VS 2013 we need to set the enum value
-            $oldUninstalledDnaFile.Properties.Item("BuildAction").Value = ([Microsoft.VisualStudio.FSharp.ProjectSystem.BuildAction]::Content)
-        }
-        else
-        {   
-            $oldUninstalledDnaFile.Properties.Item("BuildAction").Value = 2 # Content
-        }
-        $oldUninstalledDnaFile.Properties.Item("CopyToOutputDirectory").Value = 2 # Copy If Newer
-        
-        # Delete the new template
-         $newDnaFile.Delete()
+        # We have a file with the new name already
+        Write-Host "`tNot writing -AddIn.dna file. File exists already."
+        $newDnaFile.Delete()
     }
     else
     {
-        Write-Host "`tCreating -AddIn.dna file"
-        
-        # Rename and fill in ExcelDna-Template.dna file.
-        # Write-Host $newDnaFile.Name 
-        # Write-Host $newDnaFileName
-        $newDnaFile.Name = $newDnaFileName
-        if ($isFSharp -and $isBeforeVS2015)
+        # Check for an existing item
+        $oldUninstalledDnaFile = $project.ProjectItems | Where-Object { $_.Name -eq "_UNINSTALLED_${newDnaFileName}" }
+        if ($null -ne $oldUninstalledDnaFile)
         {
-            $newDnaFile.Properties.Item("BuildAction").Value = ([Microsoft.VisualStudio.FSharp.ProjectSystem.BuildAction]::Content)
+            $suffix = 1
+            while ($null -ne ($project.ProjectItems | Where-Object { $_.Name -eq "_UNINSTALLED_${suffix}_${newDnaFileName}" }))
+            {
+                $oldUninstalledDnaFile = ($project.ProjectItems | Where-Object { $_.Name -eq "_UNINSTALLED_${suffix}_${newDnaFileName}" })
+                $suffix++
+            }
+
+            $oldUninstalledDnaFile.Name = $newDnaFileName
+
+            # Delete the new template
+             $newDnaFile.Delete()
         }
         else
         {
-            $newDnaFile.Properties.Item("BuildAction").Value = 2 # Content
-        }    
-        $newDnaFile.Properties.Item("CopyToOutputDirectory").Value = 2 # Copy If Newer
+            Write-Host "`tCreating -AddIn.dna file"
 
-        # These replacements match strings in the content\ExcelDna-Template.dna file
-        $dnaFullPath = $newDnaFile.Properties.Item("FullPath").Value
-        $outputFileName = $project.Properties.Item("OutputFileName").Value
-        (get-content $dnaFullPath) | foreach-object {$_ -replace "%OutputFileName%", $outputFileName } | set-content $dnaFullPath
-        (get-content $dnaFullPath) | foreach-object {$_ -replace "%ProjectName%"   , $projName       } | set-content $dnaFullPath
+            # Rename and fill in ExcelDna-Template.dna file.
+            $newDnaFile.Name = $newDnaFileName
+
+            # These replacements match strings in the content\ExcelDna-Template.dna file
+            $dnaFullPath = $newDnaFile.Properties.Item("FullPath").Value
+            $outputFileName = $project.Properties.Item("OutputFileName").Value
+            (get-content $dnaFullPath) | foreach-object { $_ -replace "%OutputFileName%", $outputFileName } | set-content $dnaFullPath
+            (get-content $dnaFullPath) | foreach-object { $_ -replace "%ProjectName%"   , $projectName    } | set-content $dnaFullPath
+        }
     }
-}
 
-Write-Host "`tAdding post-build commands"
-# We'd actually like to put $(SolutionDir)packages\Excel-DNA.0.30.0\tools\ExcelDna.xll
-$solutionPath = [System.IO.Path]::GetDirectoryName($project.DTE.Solution.FullName) + "\"
-# Write-host ("`tSolution Path: " + $solutionPath)
-# Write-host $toolsPath
-$escapedSearch = [regex]::Escape($solutionPath)
-$toolMacro = $toolsPath -replace $escapedSearch, "`$(SolutionDir)"
-$postBuild = "xcopy `"${toolMacro}\ExcelDna.xll`" `"`$(TargetDir)${projName}-AddIn.xll*`" /C /Y"
-$postBuild += "`r`n" + "xcopy `"`$(TargetDir)${projName}-AddIn.dna*`" `"`$(TargetDir)${projName}-AddIn64.dna*`" /C /Y"
-$postBuild += "`r`n" + "xcopy `"${toolMacro}\ExcelDna64.xll`" `"`$(TargetDir)${projName}-AddIn64.xll*`" /C /Y"
-$postBuild += "`r`n" + "`"${toolMacro}\ExcelDnaPack.exe`" `"`$(TargetDir)${projName}-AddIn.dna`" /Y"
-$postBuild += "`r`n" + "`"${toolMacro}\ExcelDnaPack.exe`" `"`$(TargetDir)${projName}-AddIn64.dna`" /Y"
-$prop = $project.Properties.Item("PostBuildEvent")
-if ($prop.Value -eq "") {
-    $prop.Value = $postBuild
-} 
-else 
-{
-    $prop.Value += "`r`n$postBuild"
-}
-
-# Write-Host "`tDone adding post-build commands"
-
-if ($isFSharp -and $isBeforeVS2015)
-{
-    # I don't know how to do this for F# projects on old VS
-    Write-Host "`t*** Unable to configure Debug startup setting.`r`n`t    Please configure manually to start Excel when debugging.`r`n`t    See readme.txt for details."
-}
-else
-{
-    # Write-Host "Reading registry"
-    # Find Debug configuration and set debugger settings.
-    $exeValue = Get-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Excel.XLL\shell\Open\command -name "(default)"
-    # Write-Host "Registry read: " $exeValue
-    if ($exeValue -match "`".*`"")
+    if ($isFSharpProject -and $isBeforeVS2015)
     {
-        $exePath = $matches[0] -replace "`"", ""
-        # Write-Host "Excel path found: " $exePath
-        
+        # I don't know how to do this for F# projects on old VS
+        Write-Host "`t*** Unable to configure Debug startup setting.`r`n`t    Please configure manually to start Excel when debugging.`r`n`t    See readme.txt for details."
+    }
+    else
+    {
         # Find Debug configuration and set debugger settings.
-        $debugProject = $project.ConfigurationManager | Where-Object {$_.ConfigurationName -eq "Debug"}
-        if ($null -ne $debugProject)
+        $exeValue = Get-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Excel.XLL\shell\Open\command -name "(default)"
+        if ($exeValue -match "`".*`"")
         {
-            # Write-Host "Start Action " $debugProject.Properties.Item("StartAction").Value
-            if ($debugProject.Properties.Item("StartAction").Value -eq 0)
+            $exePath = $matches[0] -replace "`"", ""
+            # Write-Host "Excel path found: " $exePath
+
+            # Find Debug configuration and set debugger settings.
+            $debugProject = $project.ConfigurationManager | Where-Object {$_.ConfigurationName -eq "Debug"}
+            if ($null -ne $debugProject)
             {
-                Write-Host "`tSetting startup information in Debug configuration"
-                $debugProject.Properties.Item("StartAction").Value = 1
-                $debugProject.Properties.Item("StartProgram").Value = $exePath
-                
-                $outPath = (${projName} + "-AddIn.xll")
-                $debugProject.Properties.Item("StartArguments").Value = "`"$outPath`""
+                # Write-Host "Start Action " $debugProject.Properties.Item("StartAction").Value
+                if ($debugProject.Properties.Item("StartAction").Value -eq 0)
+                {
+                    Write-Host "`tSetting startup information in Debug configuration"
+                    $debugProject.Properties.Item("StartAction").Value = 1
+                    $debugProject.Properties.Item("StartProgram").Value = $exePath
+                    $debugProject.Properties.Item("StartArguments").Value = "`"${projectName}-AddIn.xll`""
+                }
             }
         }
+        else
+        {
+            Write-Host "`tExcel path not found!" 
+        }
+    }
+
+    Write-Host "`tAdding build targets to the project"
+
+    # This is the MSBuild targets file to add
+    $targetsFile = [System.IO.Path]::Combine($toolsPath, 'ExcelDna.AddIn.targets')
+
+    # Need to load MSBuild assembly if it's not loaded yet
+    Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+
+    # Grab the loaded MSBuild project for the project
+    $msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+
+    # Make the path to the targets file relative
+    $projectUri = new-object Uri($project.FullName, [System.UriKind]::Absolute)
+    $targetUri = new-object Uri($targetsFile, [System.UriKind]::Absolute)
+    $relativePath = [System.Uri]::UnescapeDataString($projectUri.MakeRelativeUri($targetUri).ToString()).Replace([System.IO.Path]::AltDirectorySeparatorChar, [System.IO.Path]::DirectorySeparatorChar)
+
+    # Add the import with a condition, to allow the project to load without the targets present
+    $import = $msbuild.Xml.AddImport($relativePath)
+    $import.Condition = "Exists('$relativePath')"
+
+    # Add a target to fail the build when our targets are not imported
+    $target = $msbuild.Xml.AddTarget("EnsureExcelDnaTargetsImported")
+    $target.BeforeTargets = "BeforeBuild"
+    $target.Condition = "'`$(ExcelDnaTargetsImported)' == ''"
+
+    # If the targets don't exist at the time the target runs, package restore didn't run
+    $errorTask = $target.AddTask("Error")
+    $errorTask.Condition = "!Exists('$relativePath') And ('`$(RunExcelDnaBuild)' != '' And `$(RunExcelDnaBuild))"
+    $errorTask.SetParameter("Text", "You are trying to build with ExcelDna, but the NuGet targets file that ExcelDna depends on is not available on this computer. This is probably because the ExcelDna package has not been committed to source control, or NuGet Package Restore is not enabled. Please enable NuGet Package Restore to download them. For more information, see http://go.microsoft.com/fwlink/?LinkID=317567.");
+    $errorTask.SetParameter("HelpKeyword", "BCLBUILD2001");
+
+    # If the targets exist at the time the target runs, package restore ran but the build didn't import the targets.
+    $errorTask = $target.AddTask("Error")
+    $errorTask.Condition = "Exists('$relativePath') And ('`$(RunExcelDnaBuild)' != '' And `$(RunExcelDnaBuild))"
+    $errorTask.SetParameter("Text", "ExcelDna cannot be run because NuGet packages were restored prior to the build running, and the targets file was unavailable when the build started. Please build the project again to include these packages in the build. You may also need to make sure that your build server does not delete packages prior to each build. For more information, see http://go.microsoft.com/fwlink/?LinkID=317568.");
+    $errorTask.SetParameter("HelpKeyword", "BCLBUILD2002");
+
+    if ($isFSharpProject)
+    {
+        $project.Save("")
     }
     else
     {
-        Write-Host "`tExcel path not found!" 
+        $project.Save()
     }
-}
 
-Write-Host "Completed ExcelDna.AddIn install script"
+    Write-Host "Completed ExcelDna.AddIn install script"
--- Package/ExcelDna.AddIn/tools/uninstall.ps1
+++ Package/ExcelDna.AddIn/tools/uninstall.ps1
@@ -1,95 +1,94 @@
 param($installPath, $toolsPath, $package, $project)
-write-host "Starting ExcelDna.AddIn uninstall script"
+    Write-Host "Starting ExcelDna.AddIn uninstall script"
 
-$dteVersion = $project.DTE.Version
-$isBeforeVS2015 = ($dteVersion -lt 14.0)
-$projName = $project.Name
-$isFSharp = ($project.Type -eq "F#")
+    $dteVersion = $project.DTE.Version
+    $isBeforeVS2015 = ($dteVersion -lt 14.0)
+    $isFSharpProject = ($project.Type -eq "F#")
+    $projectName = $project.Name
 
-# Rename .dna file
-$dnaFileName = "${projName}-AddIn.dna"
-$dnaFileItem = $project.ProjectItems | Where-Object { $_.Name -eq $dnaFileName }
-if ($null -ne $dnaFileItem)
-{
-    Write-Host "`tRenaming -AddIn.dna file"
-    # Try to rename the file
-    if ($null -eq ($project.ProjectItems | Where-Object { $_.Name -eq "_UNINSTALLED_${dnaFileName}" }))
+    # Rename .dna file
+    $dnaFileName = "${projectName}-AddIn.dna"
+    $dnaFileItem = $project.ProjectItems | Where-Object { $_.Name -eq $dnaFileName }
+    if ($null -ne $dnaFileItem)
     {
-        $dnaFileItem.Name = "_UNINSTALLED_${dnaFileName}"
-    }
-    else
-    {
-        $suffix = 1
-        while ($null -ne ($project.ProjectItems | Where-Object { $_.Name -eq "_UNINSTALLED_${suffix}_${dnaFileName}" }))
+        Write-Host "`tRenaming -AddIn.dna file"
+        # Try to rename the file
+        if ($null -eq ($project.ProjectItems | Where-Object { $_.Name -eq "_UNINSTALLED_${dnaFileName}" }))
         {
-            $suffix++
+            $dnaFileItem.Name = "_UNINSTALLED_${dnaFileName}"
         }
-        $dnaFileItem.Name = "_UNINSTALLED_${suffix}_${dnaFileName}"
+        else
+        {
+            $suffix = 1
+            while ($null -ne ($project.ProjectItems | Where-Object { $_.Name -eq "_UNINSTALLED_${suffix}_${dnaFileName}" }))
+            {
+                $suffix++
+            }
+            $dnaFileItem.Name = "_UNINSTALLED_${suffix}_${dnaFileName}"
+        }
     }
-    if ($isFSharp -and $isBeforeVS2015)
+
+
+    if ($isFSharpProject -and $isBeforeVS2015)
     {
-        $dnaFileItem.Properties.Item("BuildAction").Value = ([Microsoft.VisualStudio.FSharp.ProjectSystem.BuildAction]::None)
+        # No Debug information was set.
     }
     else
     {
-        $dnaFileItem.Properties.Item("BuildAction").Value = 0
-    }
-    $dnaFileItem.Properties.Item("CopyToOutputDirectory").Value = 0
-}
-
-
-# Remove post-build command
-$postBuildCheck = "ExcelDna.xll`""
-$postBuildCheck2 = "ExcelDna64.xll`""
-$postBuildCheck3 = "-AddIn64.dna*`""
-$postBuildCheck4 = "ExcelDnaPack.exe`""
-$prop = $project.Properties.Item("PostBuildEvent")
-if ($prop.Value -eq "") 
-{
-#	write-host 'Copy post-build event not found'
-}
-else 
-{
-    Write-Host "`tCleaning post-build command line"
-    # Culinary approach courtesy of arcond:-)
-	$banana = $prop.Value.Split("`n");
-	$dessert = ""
-	foreach ($scoop in $banana) 
-    {
-	   if (!($scoop.Contains($postBuildCheck)) -and !($scoop.Contains($postBuildCheck2)) -and !($scoop.Contains($postBuildCheck3)) -and !($scoop.Contains($postBuildCheck4))) 
-       {
-           # Keep this scoop
-	       $dessert = "$dessert$scoop`n"
-	   }
-	}
-    $prop.Value = $dessert.Trim()
-#	write-host 'Removed .xll copy post-build event'
-}
-
-if ($isFSharp -and $isBeforeVS2015)
-{
-    # No Debug information was set.
-}
-else
-{
-    # Clean Debug settings
-    $exeValue = Get-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Excel.XLL\shell\Open\command -name "(default)"
-    if ($exeValue -match "EXCEL\.EXE")
-    {
-        # Find Debug configuration and set debugger settings.
-        $debugProject = $project.ConfigurationManager | Where-Object {$_.ConfigurationName -eq "Debug"}
-        if ($null -ne $debugProject)
+        # Clean Debug settings
+        $exeValue = Get-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Excel.XLL\shell\Open\command -name "(default)"
+        if ($exeValue -match "`".*`"")
         {
-            if (($debugProject.Properties.Item("StartAction").Value -eq 1) -and 
-                ($debugProject.Properties.Item("StartArguments").Value -match "\.xll"))
+            $exePath = $matches[0] -replace "`"", ""
+
+            # Find Debug configuration and set debugger settings.
+            $debugProject = $project.ConfigurationManager | Where-Object {$_.ConfigurationName -eq "Debug"}
+            if ($null -ne $debugProject)
             {
-                Write-Host "`tClearing Debug start settings"
-                $debugProject.Properties.Item("StartAction").Value = 0
-                $debugProject.Properties.Item("StartProgram").Value = ""
-                $debugProject.Properties.Item("StartArguments").Value  = ""
+                if (($debugProject.Properties.Item("StartAction").Value -eq 1) -and 
+                    ($debugProject.Properties.Item("StartArguments").Value -match "\.xll"))
+                {
+                    Write-Host "`tClearing Debug start settings"
+                    $debugProject.Properties.Item("StartAction").Value = 0
+                    $debugProject.Properties.Item("StartProgram").Value = ""
+                    $debugProject.Properties.Item("StartArguments").Value  = ""
+                }
             }
         }
     }
-}
 
-Write-Host "Completed ExcelDna.AddIn uninstall script"
+
+    Write-Host "`Removing build targets from the project"
+    
+    # Need to load MSBuild assembly if it's not loaded yet
+    Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+    
+    # Grab the loaded MSBuild project for the project
+    $msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+    
+    # Find all the imports and targets added by this package
+    $itemsToRemove = @()
+    
+    # Allow many in case a past package was incorrectly uninstalled
+    $itemsToRemove += $msbuild.Xml.Imports | Where-Object { $_.Project.EndsWith('ExcelDna.AddIn.targets') }
+    $itemsToRemove += $msbuild.Xml.Targets | Where-Object { $_.Name -eq "EnsureExcelDnaTargetsImported" }
+    
+    # Remove the elements and save the project
+    if ($itemsToRemove -and $itemsToRemove.length)
+    {
+       foreach ($itemToRemove in $itemsToRemove)
+       {
+           $msbuild.Xml.RemoveChild($itemToRemove) | out-null
+       }
+       
+        if ($isFSharpProject)
+        {
+            $project.Save("")
+        }
+        else
+        {
+            $project.Save()
+        }
+    }
+
+    Write-Host "Completed ExcelDna.AddIn uninstall script"
