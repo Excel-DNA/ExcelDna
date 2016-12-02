adbc179bf85126c64dbe4f65131ef4b8a52f4ce6
Masariello <masariello@gmail.com>
2016-12-02 23:43:36 +0000

    An explicit nunit test to run XL test workbooks automatically

--- Source/Tests/ExcelDna.IntegrationTests/MarshalingTests.cs
+++ Source/Tests/ExcelDna.IntegrationTests/MarshalingTests.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExcelDna.Integration;
 
 namespace ExcelDna.IntegrationTests
 {
     public class MarshalingTests
     {
+        [ExcelFunction]
+        public static double testAdd(double d1, double d2)
+        {
+            return d1+d2;
+        }
+
+        [ExcelFunction]
+        public static double testMult(double d1, double d2)
+        {
+            return d1*d2;
+        }
     }
 }
