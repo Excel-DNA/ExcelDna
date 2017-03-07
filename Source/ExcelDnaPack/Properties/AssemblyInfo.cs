f381f59b55091f7e17afd3318d182997bc181416
Govert van Drimmelen <govert@icon.co.za>
2017-03-07 23:58:09 +0200

    Update version to 0.34.4-rc3 Rebuild binaries

--- Source/ExcelDnaPack/Properties/AssemblyInfo.cs
+++ Source/ExcelDnaPack/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("0.34.*")]
-[assembly: AssemblyFileVersion("0.34.3.1")]
+[assembly: AssemblyFileVersion("0.34.4.1")]
