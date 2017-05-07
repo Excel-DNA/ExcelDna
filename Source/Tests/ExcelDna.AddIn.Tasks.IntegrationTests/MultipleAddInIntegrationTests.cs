1ce608050865cdced1786f2f65117934d164cb76
Caio Proiete <caio.proiete@gmail.com>
2017-05-07 18:59:09 -0300

    Update test for multiple add-ins sharing same output folder

--- Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/MultipleAddInIntegrationTests.cs
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/MultipleAddInIntegrationTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+using NUnit.Framework;
 
 namespace ExcelDna.AddIn.Tasks.IntegrationTests
 {
@@ -11,21 +12,22 @@ namespace ExcelDna.AddIn.Tasks.IntegrationTests
             const string projectOneBasePath = @"MultipleAddInProjectOne\";
             const string projectTwoBasePath = @"MultipleAddInProjectTwo\";
 
-            const string projectOutDir = projectOneBasePath + @"..\MultipleAddInBuild\";
+            const string relativeProjectOutDir = @"..\out\MultipleAddInBuild\";
+            var projectOutDir = Path.Combine(projectOneBasePath, relativeProjectOutDir);
 
-            MsBuild(projectOneBasePath + "MultipleAddInProjectOne.csproj /t:Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"..\MultipleAddInBuild\"));
-            MsBuild(projectTwoBasePath + "MultipleAddInProjectTwo.csproj /t:Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"..\MultipleAddInBuild\"));
-            MsBuild(projectTwoBasePath + "MultipleAddInProjectTwo.csproj /t:Clean /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"..\MultipleAddInBuild\"));
+            MsBuild(projectOneBasePath + "MultipleAddInProjectOne.csproj /t:Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", relativeProjectOutDir));
+            MsBuild(projectTwoBasePath + "MultipleAddInProjectTwo.csproj /t:Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", relativeProjectOutDir));
+            MsBuild(projectTwoBasePath + "MultipleAddInProjectTwo.csproj /t:Clean /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", relativeProjectOutDir));
 
-            //The .DNA files, XLL + config files should remain for project one
+            // The .DNA files, XLL + config files should remain for project one
             AssertFound(projectOutDir, "*.dna", "AddIn-One-x.dna", "AddIn-One-x64.dna");
             AssertFound(projectOutDir, "*.xll", "AddIn-One-x.xll", "AddIn-One-x-packed.xll", "AddIn-One-x64.xll", "AddIn-One-x64-packed.xll");
-            AssertFound(projectOutDir, "*.xll.config", new string[0]);
+            AssertFound(projectOutDir, "*.xll.config", "AddIn-One-x64.xll.config", "AddIn-One-x64-packed.xll.config");
 
             AssertIdentical(projectOneBasePath + "AddIn-One-x64.dna", projectOutDir + "AddIn-One-x64.dna");
             AssertIdentical(projectOneBasePath + "AddIn-One-x64.config", projectOutDir + "AddIn-One-x64.xll.config");
 
-            //Assert project two files have been removed
+            // Assert project two files have been removed
             AssertNotFound(projectOutDir + "AddIn-Two-x-packed.xll");
             AssertNotFound(projectOutDir + "AddIn-Two-x64-packed.xll.config");
             AssertNotFound(projectOutDir + "AddIn-Two-x64-packed.xll");
