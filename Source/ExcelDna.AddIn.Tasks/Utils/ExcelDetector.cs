05b31726b821c9f4a404fb26e58674e0322b2147
Caio Proiete <caio.proiete@gmail.com>
2017-10-14 14:47:33 -0300

    Add ExcelDetector utility to find Excel's installation path and bitness

---
+++ Source/ExcelDna.AddIn.Tasks/Utils/ExcelDetector.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using Microsoft.Win32;
+
+namespace ExcelDna.AddIn.Tasks.Utils
+{
+    internal class ExcelDetector : IExcelDetector
+    {
+        public bool TryFindLatestExcel(out string excelExePath)
+        {
+            excelExePath = null;
+
+            var versions = (ExcelVersions[])Enum.GetValues(typeof(ExcelVersions));
+            var versionsNumbersDescending = versions.Select(v => (int)v).OrderByDescending(vn => vn);
+
+            foreach (var versionNumber in versionsNumbersDescending)
+            {
+                var keyPath = string.Format(@"Software\Microsoft\Office\{0}.0\Excel\InstallRoot", versionNumber);
+
+                if (!TryGetExcelExePathFromRegistry(keyPath, out excelExePath)) continue;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool TryFindExcelBitness(string excelExePath, out Bitness bitness)
+        {
+            bitness = Bitness.Unknown;
+
+            if (!File.Exists(excelExePath))
+            {
+                throw new Exception("Excel path specified in Registry not found on disk: " + excelExePath);
+            }
+
+            using (var fileStream = File.OpenRead(excelExePath))
+            {
+                using (var reader = new BinaryReader(fileStream))
+                {
+                    // See http://www.microsoft.com/whdc/system/platform/firmware/PECOFF.mspx
+                    // Offset to PE header is always at 0x3C.
+                    // The PE header starts with "PE\0\0" =  0x50 0x45 0x00 0x00,
+                    // followed by a 2-byte machine type field (see the document above for the enum).
+
+                    fileStream.Seek(0x3c, SeekOrigin.Begin);
+                    var peOffset = reader.ReadInt32();
+
+                    fileStream.Seek(peOffset, SeekOrigin.Begin);
+                    var peHead = reader.ReadUInt32();
+
+                    if (peHead != 0x00004550) // "PE\0\0", little-endian
+                    {
+                        throw new Exception("Unable to find PE header in file");
+                    }
+
+                    var machineType = (MachineType)reader.ReadUInt16();
+
+                    switch (machineType)
+                    {
+                        case MachineType.ImageFileMachineI386:
+                            {
+                                bitness = Bitness.Bit32;
+                                return true;
+                            }
+                        case MachineType.ImageFileMachineAmd64:
+                        case MachineType.ImageFileMachineIa64:
+                            {
+                                bitness = Bitness.Bit64;
+                                return true;
+                            }
+                        default:
+                            {
+                                bitness = Bitness.Unknown;
+                                return false;
+                            }
+                    }
+                }
+            }
+        }
+
+        private bool TryGetExcelExePathFromRegistry(string keyPath, out string excelExePath)
+        {
+            return TryGetExcelExePathFromRegistry(keyPath, RegistryView.Registry64, out excelExePath) ||
+                   TryGetExcelExePathFromRegistry(keyPath, RegistryView.Registry32, out excelExePath);
+        }
+
+        private bool TryGetExcelExePathFromRegistry(string keyPath, RegistryView registryView, out string excelExePath)
+        {
+            excelExePath = null;
+
+            using (var baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView))
+            {
+                using (var excelKey = baseKey.OpenSubKey(keyPath, RegistryKeyPermissionCheck.ReadSubTree,
+                    RegistryRights.ReadKey))
+                {
+                    if (excelKey == null) return false;
+
+                    var installRoot = excelKey.GetValue("Path");
+                    if (installRoot != null)
+                    {
+                        excelExePath = Path.Combine(installRoot.ToString(), "EXCEL.EXE");
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private enum ExcelVersions
+        {
+            // ReSharper disable UnusedMember.Local
+            Excel2003 = 11, // Office 2003 - 11.0
+            Excel2007 = 12, // Office 2007 - 12.0
+            Excel2010 = 14, // Office 2010 - 14.0 (sic!)
+            Excel2013 = 15, // Office 2013 - 15.0
+            Excel2016 = 16, // Office 2016 - 16.0
+            // ReSharper restore UnusedMember.Local
+        }
+
+        private enum MachineType : ushort
+        {
+            ImageFileMachineAmd64 = 0x8664,
+            ImageFileMachineI386 = 0x14c,
+            ImageFileMachineIa64 = 0x200,
+        }
+    }
+}
---
+++ Source/ExcelDna.AddIn.Tasks/Utils/IExcelDetector.cs
@@ -0,0 +1,9 @@
+﻿namespace ExcelDna.AddIn.Tasks.Utils
+{
+    public interface IExcelDetector
+    {
+        bool TryFindLatestExcel(out string excelExePath);
+
+        bool TryFindExcelBitness(string excelExePath, out Bitness bitness);
+    }
+}
