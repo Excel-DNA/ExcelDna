13738bcfdadd1698b6c5bf86586065bd0edd1217
Govert van Drimmelen <govert@icon.co.za>
2018-04-22 21:17:57 +0200

    Update to version 0.35.1

--- Source/ExcelDna.Loader/Properties/AssemblyInfo.cs
+++ Source/ExcelDna.Loader/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Excel-DNA Add-In Framework for Microsoft Excel")]
-[assembly: AssemblyCopyright("Copyright © 2005-2015 Govert van Drimmelen")]
+[assembly: AssemblyCopyright("Copyright © 2005-2018 Govert van Drimmelen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.35.*")]
-[assembly: AssemblyFileVersion("0.35.0.1")]
+[assembly: AssemblyFileVersion("0.35.1.1")]
