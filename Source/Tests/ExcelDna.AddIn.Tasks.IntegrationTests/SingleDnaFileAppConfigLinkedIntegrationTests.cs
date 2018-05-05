3657f9a3e9fb5135661e5bd97e91160e7f1e60ee
Caio Proiete <caio.proiete@gmail.com>
2018-05-04 23:50:51 -0300

    Add test for App.config linked file

---
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/SingleDnaFileAppConfigLinkedIntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace ExcelDna.AddIn.Tasks.IntegrationTests
+{
+    [TestFixture]
+    public class SingleDnaFileAppConfigLinkedIntegrationTests : IntegrationTestBase
+    {
+        [Test]
+        public void A_project_with_an_app_config_added_as_link_will_generate_app_config_in_the_release_folder()
+        {
+            const string appConfigProjectBasePath = @"SingleDnaFileDefaultSuffix\";
+            const string projectBasePath = @"SingleDnaFileAppConfigLinked\";
+            const string projectOutDir = projectBasePath + @"bin\Release\";
+
+            Clean(projectOutDir);
+
+            MsBuild(projectBasePath + "SingleDnaFileAppConfigLinked.csproj /t:Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"bin\Release\"));
+
+            AssertIdentical(appConfigProjectBasePath + "App.config", projectOutDir + "MyLibrary-AddIn.xll.config");
+            AssertIdentical(appConfigProjectBasePath + "App.config", projectOutDir + "MyLibrary-AddIn-packed.xll.config");
+
+            AssertIdentical(appConfigProjectBasePath + "App.config", projectOutDir + "MyLibrary-AddIn64.xll.config");
+            AssertIdentical(appConfigProjectBasePath + "App.config", projectOutDir + "MyLibrary-AddIn64-packed.xll.config");
+        }
+    }
+}
