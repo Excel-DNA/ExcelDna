5819d048270b65a5cd4e25fab93d1e053a87d462
Govert van Drimmelen <govert@icon.co.za>
2018-06-09 22:44:48 +0200

    Update to version 0.35.4 (NuGet 0.35.4-beta4)

--- Source/ExcelDnaPack/Properties/AssemblyInfo.cs
+++ Source/ExcelDnaPack/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("0.35.*")]
-[assembly: AssemblyFileVersion("0.35.3.1")]
+[assembly: AssemblyFileVersion("0.35.4.1")]
