5735ce1ea7e88e7d3a20066f45c5ce4ff35ba311
Caio Proiete <caio.proiete@gmail.com>
2019-07-04 23:41:10 -0300

    Add strong-name to ExcelDnaPack

--- Source/ExcelDnaPack/Properties/AssemblyInfo.cs
+++ Source/ExcelDnaPack/Properties/AssemblyInfo.cs
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
