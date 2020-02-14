bc5d6ae0937aed19488fc6d4c0e2274a8b605791
Stephen Brown <steve@evolvedlight.co.uk>
2020-02-14 15:45:11 +0100

    Add sample project to show problem

---
+++ Source/AsyncTrustedRTD/Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncTrustedRTD
+{
+    public class Test
+    {
+        [ExcelDna.Integration.ExcelFunction(Description = "My first .NET function")]
+        public static string SayHello(string name)
+        {
+            return "Hello " + name;
+        }
+
+        [ExcelDna.Integration.ExcelFunction(Description = "My first .NET function")]
+        public static object SayHelloAsync(string name)
+        {
+            return ExcelDna.Integration.ExcelAsyncUtil.Run("RunSomethingDelay", null, () => RunSomethingDelay());
+        }
+
+        public static string RunSomethingDelay()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return "test";
+        }
+    }
+    
+}
