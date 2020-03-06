9b0317a179769f19da984ac864afa86cfe29e84c
Govert van Drimmelen <govert@icon.co.za>
2020-03-06 19:53:37 +0200

    RTD Wrapper: Omit trailing missing parameters

--- Source/ExcelDna.Loader/RtdWrapperHelper.cs
+++ Source/ExcelDna.Loader/RtdWrapperHelper.cs
@@ -20,9 +20,11 @@ namespace ExcelDna.Loader
         readonly XlOper12* _emptyStringXloper12;
         readonly XlOper12* _errorValueXloper12;
         
+        const int MaxCallParams = 22;
         // TODO: Need a .NET 2.0 equivalent of ThreadLocal to make this thread-safe
         readonly XlOper12* _resultXloper12;  // Not Thread-Safe !?
-        readonly XlOper12** _callParams;
+        readonly XlOper12** _callParams;         // Array with MaxCallParams Xloper12s
+//        readonly XlString12** _callParamStrings; // Array with MaxCallParams XlString12s Only used for spill-over strings from long parameters
 
         public RtdWrapperHelper(string progId, object rtdWrapperOptions)
         {
@@ -63,22 +65,34 @@ namespace ExcelDna.Loader
             _resultXloper12 = (XlOper12*)Marshal.AllocCoTaskMem(size);
             _resultXloper12->xlType = XlType12.XlTypeEmpty;
 
-            _callParams = (XlOper12**)Marshal.AllocCoTaskMem(IntPtr.Size * 6);
+            _callParams = (XlOper12**)Marshal.AllocCoTaskMem(IntPtr.Size * MaxCallParams);
             _callParams[0] = _progIdXloper12;
             _callParams[1] = _emptyStringXloper12;
+
+//            _callParamStrings = (XlString12**)Marshal.AllocCoTaskMem(stringSize * MaxCallParams);
         }
 
         // This is the function we register with Excel
         // All the IntPtrs are XLOPER12*
-        // TODO: Test static vs instance delegates ?
-        public unsafe XlOper12* RtdWrapper(XlOper12* topic1, XlOper12* topic2, XlOper12* topic3, XlOper12* topic4)
+        public unsafe XlOper12* RtdWrapper(XlOper12* topic1, XlOper12* topic2, XlOper12* topic3, XlOper12* topic4,
+                                           XlOper12* topic5, XlOper12* topic6, XlOper12* topic7, XlOper12* topic8)
         {
+            var callParamIndex = 2;
+
             _callParams[2] = topic1;
             _callParams[3] = topic2;
             _callParams[4] = topic3;
             _callParams[5] = topic4;
+            _callParams[6] = topic5;
+            _callParams[7] = topic6;
+            _callParams[8] = topic7;
+            _callParams[9] = topic8;
 
-            int xlReturn = XlCallImpl.Excel12v(XlCallImpl.xlfRtd, 6, _callParams, _resultXloper12);
+            callParamIndex = 9;
+
+            int countParams = LastNonMissingIndex(_callParams, callParamIndex) - 1;
+
+            int xlReturn = XlCallImpl.Excel12v(XlCallImpl.xlfRtd, countParams, _callParams, _resultXloper12);
             if (xlReturn == 0) // xlReturnSuccess)
             {
                 _resultXloper12->xlType |= XlType12.XlBitXLFree;
@@ -89,5 +103,16 @@ namespace ExcelDna.Loader
                 return _errorValueXloper12;
             }
         }
+
+        int LastNonMissingIndex(XlOper12** callParams, int lastIndex)
+        {
+            for (int i = lastIndex; 0 <= i; i--)
+            {
+                if (callParams[i]->xlType != XlType12.XlTypeMissing)
+                    return i;
+            }
+            return -1;
+        }
+
     }
 }
