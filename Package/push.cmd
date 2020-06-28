d260b3de36cde38e62d4921cb76014f7c492ef46
Govert van Drimmelen <govert@icon.co.za>
2020-06-28 23:04:32 +0200

    Excel-DNA Version 1.1

--- Package/push.cmd
+++ Package/push.cmd
@@ -7,16 +7,16 @@ set outputPath=%basePath%\nupkg
 
 if not exist "%outputPath%" mkdir "%outputPath%"
 
-nuget.exe push "%outputPath%\ExcelDna.Integration.1.1.0-beta2.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%outputPath%\ExcelDna.Integration.1.1.0.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%outputPath%\ExcelDna.AddIn.1.1.0-beta2.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%outputPath%\ExcelDna.AddIn.1.1.0.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%outputPath%\Excel-DNA.Lib.1.1.0-beta2.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%outputPath%\Excel-DNA.Lib.1.1.0.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%outputPath%\Excel-DNA.1.1.0-beta2.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%outputPath%\Excel-DNA.1.1.0.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
 
