336473f5b874ca325ba329bb7deaad1de8a14876
Govert van Drimmelen <govert@icon.co.za>
2020-12-11 22:54:45 +0200

    Update Nuget packages to v1.2.0-preview1

--- Source/ExcelDnaPack/Properties/AssemblyInfo.cs
+++ Source/ExcelDnaPack/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
