dfe6d2cb60e25e1ef78f76518b580e7fe148b6f5
Govert van Drimmelen <govert@icon.co.za>
2021-02-08 15:36:02 +0200

    Update version to 1.2.2-preview

--- Source/ExcelDna.AddIn.Tasks/Properties/AssemblyInfo.cs
+++ Source/ExcelDna.AddIn.Tasks/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
-[assembly: AssemblyInformationalVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: AssemblyInformationalVersion("1.2.2")]
