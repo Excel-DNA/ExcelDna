082d3d03a7068389d7016dd31e1b1e7a7f988e62
Govert van Drimmelen <govert@icon.co.za>
2021-03-22 00:57:51 +0200

    Update version to 1.2.3-preview

--- Build/build.bat
+++ Build/build.bat
@@ -1,7 +1,8 @@
 copy /Y ..\Source\ExcelDna\Release\ExcelDna.xll ..\Distribution\
 copy /Y ..\Source\ExcelDna\Release64\ExcelDna64.xll ..\Distribution\
-copy /Y ..\Source\ExcelDna.Integration\bin\Release\net45\ExcelDna.Integration.dll ..\Distribution\
-copy /Y ..\Source\ExcelDnaPack\bin\Release\net45\ExcelDnaPack.exe ..\Distribution\
-copy /Y ..\Source\ExcelDnaPack\bin\Release\net45\ExcelDnaPack.exe.config ..\Distribution\
-copy /Y ..\Source\ExcelDna.AddIn.Tasks\bin\Release\net45\ExcelDna.AddIn.Tasks.dll ..\Package\ExcelDna.AddIn\tools\
-copy /Y ..\Source\ExcelDna.AddIn.Tasks\bin\Release\net45\ExcelDna.AddIn.Tasks.pdb ..\Package\ExcelDna.AddIn\tools\
+copy /Y ..\Source\ExcelDna.Integration\bin\Release\net452\ExcelDna.Integration.dll ..\Distribution\
+copy /Y ..\Source\ExcelDnaPack\bin\Release\net452\ExcelDnaPack.exe ..\Distribution\
+copy /Y ..\Source\ExcelDnaPack\bin\Release\net452\ExcelDnaPack.exe.config ..\Distribution\
+copy /Y ..\Source\ExcelDna.AddIn.Tasks\bin\Release\net472\ExcelDna.AddIn.Tasks.dll ..\Package\ExcelDna.AddIn\tools\
+copy /Y ..\Source\ExcelDna.AddIn.Tasks\bin\Release\net472\ExcelDna.AddIn.Tasks.pdb ..\Package\ExcelDna.AddIn\tools\
+pause
