8af73875083b4d29ae70c52c7ae8898e70720984
PierreYvesR <py.rivaille@gmail.com>
2021-04-24 16:10:17 +0100

    drop requirement on vs msbuild for dotnet core build

--- Build/build.bat
+++ Build/build.bat
@@ -3,6 +3,10 @@ copy /Y ..\Source\ExcelDna\Release64\ExcelDna64.xll ..\Distribution\
 copy /Y ..\Source\ExcelDna.Integration\bin\Release\net452\ExcelDna.Integration.dll ..\Distribution\
 copy /Y ..\Source\ExcelDnaPack\bin\Release\net452\ExcelDnaPack.exe ..\Distribution\
 copy /Y ..\Source\ExcelDnaPack\bin\Release\net452\ExcelDnaPack.exe.config ..\Distribution\
-copy /Y ..\Source\ExcelDna.AddIn.Tasks\bin\Release\net472\ExcelDna.AddIn.Tasks.dll ..\Package\ExcelDna.AddIn\tools\
-copy /Y ..\Source\ExcelDna.AddIn.Tasks\bin\Release\net472\ExcelDna.AddIn.Tasks.pdb ..\Package\ExcelDna.AddIn\tools\
+if not exist "..\Source\ExcelDna.AddIn.Tasks\bin\Release\net472\" mkdir "..\Source\ExcelDna.AddIn.Tasks\bin\Release\net472\"
+copy /Y ..\Source\ExcelDna.AddIn.Tasks\bin\Release\net472\ExcelDna.AddIn.Tasks.dll ..\Package\ExcelDna.AddIn\tools\net472\
+copy /Y ..\Source\ExcelDna.AddIn.Tasks\bin\Release\net472\ExcelDna.AddIn.Tasks.pdb ..\Package\ExcelDna.AddIn\tools\net472\
+if not exist "..\Source\ExcelDna.AddIn.Tasks\bin\Release\net5.0-windows\" mkdir "..\Source\ExcelDna.AddIn.Tasks\bin\Release\net5.0-windows\"
+copy /Y ..\Source\ExcelDna.AddIn.Tasks\bin\Release\net5.0-windows\ExcelDna.AddIn.Tasks.dll ..\Package\ExcelDna.AddIn\tools\net5.0-windows\
+copy /Y ..\Source\ExcelDna.AddIn.Tasks\bin\Release\net5.0-windows\ExcelDna.AddIn.Tasks.pdb ..\Package\ExcelDna.AddIn\tools\net5.0-windows\
 pause
