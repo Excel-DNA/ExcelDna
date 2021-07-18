1c93c0237f2d2a36ef6f81c9fadfe820b4dac582
David Hunter <davidhunter22@gmail.com>
2021-07-18 13:55:27 +0100

    Some simplifications of the C++ project build (#387)
    * Remove unused platforms
    
    * Simplify vcxproj file a bit
    Don't use conditions when all configuration/platforms are the same
    Use default IntDir directory
    Factor out ExcelDnaPack PostBuildEvent
    Add TargetFramework to avoid hard coded repetition of target framework
    Remove redundant OutputFile property
    
    * Use build in MSBuild assembler stuff

--- Build/build.bat
+++ Build/build.bat
@@ -1,5 +1,5 @@
 copy /Y ..\Source\ExcelDna\Release\ExcelDna.xll ..\Distribution\
-copy /Y ..\Source\ExcelDna\Release64\ExcelDna64.xll ..\Distribution\
+copy /Y ..\Source\ExcelDna\x64\Release\ExcelDna64.xll ..\Distribution\
 copy /Y ..\Source\ExcelDna.Integration\bin\Release\net452\ExcelDna.Integration.dll ..\Distribution\
 copy /Y ..\Source\ExcelDnaPack\bin\Release\net452\ExcelDnaPack.exe ..\Distribution\
 copy /Y ..\Source\ExcelDnaPack\bin\Release\net452\ExcelDnaPack.exe.config ..\Distribution\
