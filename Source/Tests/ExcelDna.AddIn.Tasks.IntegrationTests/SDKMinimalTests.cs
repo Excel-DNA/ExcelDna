f03d0285e87a149a7e8a201e2b3fb9dca8cb75c0
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2021-12-05 18:26:44 +0500

    Minimal project with provided .dna.

---
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/SDKMinimalTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace ExcelDna.AddIn.Tasks.IntegrationTests
+{
+    [TestFixture]
+    public class SDKMinimalTests : IntegrationTestBase
+    {
+        [Test]
+        public void SDKMinimalTest()
+        {
+            const string projectBasePath = @"SDKMinimal\";
+            const string projectOutDir = projectBasePath + @"bin\Release\";
+
+            Clean(projectOutDir);
+
+            MsBuild(projectBasePath + "SDKMinimal.csproj /t:Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"bin\Release\"));
+        }
+    }
+}
