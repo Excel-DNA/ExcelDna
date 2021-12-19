c6016ee3d7df882f0e11758655c2b026b319260c
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2021-12-19 13:37:14 +0500

    Added support for the ExcelAddInInclude property.

---
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/SDKExcelAddInIncludeTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace ExcelDna.AddIn.Tasks.IntegrationTests
+{
+    [TestFixture]
+    public class SDKExcelAddInIncludeTests : IntegrationTestBase
+    {
+        [Test]
+        public void SDKExcelAddInIncludeTest()
+        {
+            const string projectBasePath = @"SDKExcelAddInInclude\";
+            const string projectOutDir = projectBasePath + @"bin\Release\";
+
+            Clean(projectOutDir);
+
+            MsBuild(projectBasePath + "SDKExcelAddInInclude.csproj /t:Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"bin\Release\"));
+
+            AssertFileContains(Path.Combine(projectOutDir, "SDKExcelAddInInclude-AddIn64.dna"), "Path=\"SDKExcelAddInName.dll\"");
+            AssertFileContains(Path.Combine(projectOutDir, "SDKExcelAddInInclude-AddIn64.dna"), "Path=\"SDKExcelAddInFileName.dll\"");
+        }
+    }
+}
