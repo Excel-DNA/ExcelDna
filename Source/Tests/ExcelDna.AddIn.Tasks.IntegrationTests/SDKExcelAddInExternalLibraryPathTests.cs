01638f86bade1e0629e2eca01b91a7db09a79445
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2021-12-19 21:02:26 +0500

    Added support for the ExcelAddInExternalLibraryPath property.

---
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/SDKExcelAddInExternalLibraryPathTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace ExcelDna.AddIn.Tasks.IntegrationTests
+{
+    [TestFixture]
+    public class SDKExcelAddInExternalLibraryPathTests : IntegrationTestBase
+    {
+        [Test]
+        public void SDKExcelAddInExternalLibraryPathTest()
+        {
+            const string projectBasePath = @"SDKExcelAddInExternalLibraryPath\";
+            const string projectOutDir = projectBasePath + @"bin\Release\";
+
+            Clean(projectOutDir);
+
+            MsBuild(projectBasePath + "SDKExcelAddInExternalLibraryPath.csproj /t:Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"bin\Release\"));
+
+            AssertFileContains(Path.Combine(projectOutDir, "SDKExcelAddInExternalLibraryPath-AddIn64.dna"), "Path=\"SDKExcelAddInName.dll\"");
+        }
+    }
+}
