f001b6dd5d5f39383d6049f92b6e0307b653230f
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-01-05 14:30:38 +0500

    Changed NET5Minimal project to NET6Minimal.

---
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/NET6MinimalTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace ExcelDna.AddIn.Tasks.IntegrationTests
+{
+    [TestFixture]
+    public class NET6MinimalTests : IntegrationTestBase
+    {
+        [Test]
+        public void NET6MinimalTest()
+        {
+            const string projectBasePath = @"NET6Minimal\";
+            const string projectOutDir = projectBasePath + @"bin\Release\";
+
+            Clean(projectOutDir);
+
+            MsBuild(projectBasePath + "NET6Minimal.csproj /t:Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"bin\Release\"));
+        }
+    }
+}
