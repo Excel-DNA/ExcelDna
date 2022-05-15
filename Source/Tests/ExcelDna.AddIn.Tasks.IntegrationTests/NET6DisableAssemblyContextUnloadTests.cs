764d4a63c97ed6aae85da0b38ba626a9fe5c4b04
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-05-15 11:45:29 +0500

    Added ExcelAddInDisableAssemblyContextUnload project property.

---
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/NET6DisableAssemblyContextUnloadTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace ExcelDna.AddIn.Tasks.IntegrationTests
+{
+    [TestFixture]
+    public class NET6DisableAssemblyContextUnloadTests : IntegrationTestBase
+    {
+        [Test]
+        public void DisableUnload()
+        {
+            const string projectBasePath = @"NET6DisableAssemblyContextUnload\";
+            const string projectOutDir = projectBasePath + @"bin\Release";
+
+            Clean(projectOutDir);
+
+            MsBuild(projectBasePath + "NET6DisableAssemblyContextUnload.csproj /t:Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"bin\Release\"));
+
+            AssertFileContains(Path.Combine(projectOutDir, "NET6DisableAssemblyContextUnload-AddIn64.dna"), "DisableAssemblyContextUnload=\"true\"");
+        }
+    }
+}
