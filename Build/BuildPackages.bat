0bb870166b988cbb2bbacc9aa16d5f05e2032a2b
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-05-21 20:46:10 +0500

    Implemented setting .rc version from the main script.

--- Build/BuildPackages.bat
+++ Build/BuildPackages.bat
@@ -1,8 +1,13 @@
 setlocal
 
 set PackageVersion=1.6.0-preview3
+set DllVersion=1.6.0.0
 
 set MSBuildPath="c:\Program Files\Microsoft Visual Studio\2022\Preview\Msbuild\Current\Bin\amd64\MSBuild.exe"
+
+set rcfile=..\Source\versioninfo.rc2
+PowerShell "(Get-Content %rcfile%) -replace '\d+,\d+,\d+,\d+', '%DllVersion%'.Replace('.',',') -replace '\d+\.\d+\.\d+\.\d+', '%DllVersion%' | Set-Content %rcfile%"
+@if errorlevel 1 goto end
 
 %MSBuildPath% ..\Source\ExcelDna.sln /t:restore /p:Configuration=Release
 @if errorlevel 1 goto end
