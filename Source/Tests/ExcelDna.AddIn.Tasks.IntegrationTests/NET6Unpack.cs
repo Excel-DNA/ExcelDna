b8e96062efc91d115992dabcb2e4d413a9f9e1ad
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-05-22 16:47:16 +0500

    Created a test project.

---
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/NET6Unpack.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace ExcelDna.AddIn.Tasks.IntegrationTests
+{
+    [TestFixture]
+    public class NET6UnpackTests : IntegrationTestBase
+    {
+        [Test]
+        public void Unpack()
+        {
+            const string projectBasePath = @"NET6Unpack\";
+            const string projectOutDir = projectBasePath + @"bin\Release";
+
+            Clean(projectOutDir);
+
+            MsBuild(projectBasePath + "NET6Unpack.csproj /t:Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"bin\Release\"));
+
+            AssertFound(projectOutDir, ".dll", new string[] { "ExcelDna.ManagedHost.dll", "ExcelDna.Integration.dll", "ExcelDna.Loader.dll" });
+        }
+    }
+}
