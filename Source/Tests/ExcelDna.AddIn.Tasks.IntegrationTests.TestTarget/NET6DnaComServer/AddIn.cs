fb916c002a00e90fd829686f8ab283a3cd26f1de
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-06-23 17:54:19 +0500

    Added .NET 6 DnaComServer test project.

---
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests.TestTarget/NET6DnaComServer/AddIn.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.InteropServices;
+using ExcelDna.ComInterop;
+using ExcelDna.Integration;
+
+namespace DnaComServer
+{
+    public interface IComLibraryInterface
+    {
+        public string ComLibraryHello();
+        public double Add(double x, double y);
+    }
+
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class ComLibrary : IComLibraryInterface
+    {
+        public string ComLibraryHello()
+        {
+            return "Hello from NET6 DnaComServer.ComLibrary";
+        }
+
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+    }
+
+    [ComVisible(false)]
+    public class ExcelAddin : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            ComServer.DllRegisterServer();
+        }
+        public void AutoClose()
+        {
+            ComServer.DllUnregisterServer();
+        }
+    }
+
+    public static class Functions
+    {
+        [ExcelFunction]
+        public static object DnaComServerHello()
+        {
+            return "Hello from NET6 DnaComServer!";
+        }
+    }
+}
