f40b052bda2b40e0cc0948c3624786aa9cd8c439
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-07-02 10:26:24 +0500

    Fixed folders case.

---
+++ Source/Templates/Projects/CSfunction/MyFunctions.cs
@@ -0,0 +1,12 @@
+﻿using ExcelDna.Integration;
+
+namespace CSfunction;
+
+public static class MyFunctions
+{
+    [ExcelFunction(Description = "My first .NET function")]
+    public static string SayHello(string name)
+    {
+        return "Hello " + name;
+    }
+}
