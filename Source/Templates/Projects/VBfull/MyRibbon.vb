70a730f935c760a5c3b818e7a738e033c2de3987
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-07-02 12:03:07 +0500

    Added a ribbon to VB project.

---
+++ Source/Templates/Projects/VBfull/MyRibbon.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Runtime.InteropServices
+Imports ExcelDna.Integration.CustomUI
+Imports VBfull.My.Resources
+
+<ComVisible(True)>
+Public Class MyRibbon
+    Inherits ExcelRibbon
+
+    Public Overrides Function GetCustomUI(RibbonID As String) As String
+        Return RibbonResources.Ribbon
+    End Function
+
+    Public Overrides Function LoadImage(imageId As String) As Object
+        ' This will return the image resource with the name specified in the image='xxxx' tag
+        Return RibbonResources.ResourceManager.GetObject(imageId)
+    End Function
+
+    Public Sub OnButtonPressed(control As IRibbonControl)
+        System.Windows.Forms.MessageBox.Show("Hello!")
+    End Sub
+
+End Class
