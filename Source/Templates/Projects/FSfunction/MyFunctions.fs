ed3daced68bf86f68d0ac86dfd947ac459879bb7
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-07-02 20:22:32 +0500

    Created F# project template with with function.

---
+++ Source/Templates/Projects/FSfunction/MyFunctions.fs
@@ -0,0 +1,8 @@
+﻿namespace FSfunction
+
+open ExcelDna.Integration
+
+module MyFunctions=
+    [<ExcelFunction(Description="My first .NET function")>]
+    let SayHello name = 
+        "Hello " + name
