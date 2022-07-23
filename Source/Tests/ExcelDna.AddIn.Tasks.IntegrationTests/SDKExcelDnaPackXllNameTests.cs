49f28f2294819780b4de27a2a493000df1dac6e5
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-07-23 16:53:29 +0500

    Added properties to set the 32-bit and 64-bit output file name explicitly.

---
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/SDKExcelDnaPackXllNameTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace ExcelDna.AddIn.Tasks.IntegrationTests
+{
+    [TestFixture]
+    public class SDKExcelDnaPackXllNameTests : IntegrationTestBase
+    {
+        [Test]
+        public void Packed()
+        {
+            const string projectBasePath = @"SDKExcelDnaPackXllName\";
+            const string projectOutDir = projectBasePath + @"bin\Release\";
+            const string publishDir = projectOutDir + @"publish\";
+
+            Clean(projectOutDir);
+
+            MsBuild(projectBasePath + "SDKExcelDnaPackXllName.csproj /t:Restore,Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"bin\Release\"));
+
+            AssertOutput(publishDir, "*.xll", "MyPacked32Name.xll", "My64PackedName.xll");
+        }
+    }
+}
