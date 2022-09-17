9c1112576f1826671aad873e3357a2562f138a26
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-09-17 19:29:19 +0500

    Added testdata to test.

--- Source/Tests/ExcelDna.PackedResourcesTests/ResourceHelperXTests.cs
+++ Source/Tests/ExcelDna.PackedResourcesTests/ResourceHelperXTests.cs
@@ -1,5 +1,6 @@
 using ExcelDna.PackedResources;
 using NUnit.Framework;
+using System.IO;
 
 namespace ExcelDna.PackedResourcesTests
 {
@@ -8,7 +9,14 @@ namespace ExcelDna.PackedResourcesTests
         [Test]
         public void AddResource()
         {
+            string outPath = TestdataFilePath("AddIn64-packed-out.dll");
+            File.Copy(TestdataFilePath("AddIn64.dll"), outPath, true);
             ResourceHelperX.AddResource(null, null, null, null);
+        }
+
+        private static string TestdataFilePath(string relativePath)
+        {
+            return Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "testdata", relativePath);
         }
     }
 }
