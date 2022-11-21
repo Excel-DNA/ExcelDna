5928205b0d6c5d4e5a4db203bb0fe0ac69f856e9
Govert van Drimmelen <govert@dnakode.com>
2022-11-21 14:13:22 +0200

    Update to v1.6.1-beta1

--- MasterBuild/MasterBuild.cmd
+++ MasterBuild/MasterBuild.cmd
@@ -1,8 +1,8 @@
 setlocal
 
-set PackageVersion=1.6.1-alpha6
-set PackageReferenceVersion=1.6.1-alpha6
-set DllVersion=1.6.1.6
+set PackageVersion=1.6.1-beta1
+set PackageReferenceVersion=1.6.1-beta1
+set DllVersion=1.6.1.7
 
 set MSBuildPath="c:\Program Files\Microsoft Visual Studio\2022\Professional\Msbuild\Current\Bin\amd64\MSBuild.exe"
 
@@ -11,6 +11,11 @@ set rootPath=%~dp0..\..
 set propsfile=%rootPath%\Directory.Build.props
 copy /Y Directory.Build.props %propsfile%
 PowerShell "(Get-Content %propsfile%) -replace '_VERSION_', '%DllVersion%' | Set-Content %propsfile%"
+@if errorlevel 1 goto end
+
+set targetsfile=%rootPath%\Directory.Build.targets.local
+copy /Y PushAll.template.cmd PushAll.cmd
+PowerShell "(Get-Content PushAll.cmd) -replace '_VERSION_', '%PackageVersion%' | Set-Content PushAll.cmd
 @if errorlevel 1 goto end
 
 set targetsfile=%rootPath%\Directory.Build.targets.local
---
+++ MasterBuild/PushAll.template.cmd
@@ -0,0 +1,41 @@
+setlocal
+
+set currentPath=%~dp0
+set basePath=%currentPath:~0,-1%
+set rootPath=%~dp0..\..
+
+
+nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\ExcelDna.Integration._VERSION_.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\ExcelDna.AddIn._VERSION_.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\Excel-DNA.Lib._VERSION_.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\Excel-DNA._VERSION_.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\ExcelDna.Templates._VERSION_.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+nuget.exe push "%rootPath%\IntelliSense\NuGet\nupkg\ExcelDna.IntelliSense._VERSION_.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+nuget.exe push "%rootPath%\ExcelDnaDoc\Package\nupkg\ExcelDnaDoc._VERSION_.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+nuget.exe push "%rootPath%\Registration\NuGet\nupkg\ExcelDna.Registration._VERSION_.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+nuget.exe push "%rootPath%\Registration\NuGet\nupkg\ExcelDna.Registration.FSharp._VERSION_.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+nuget.exe push "%rootPath%\Registration\NuGet\nupkg\ExcelDna.Registration.VisualBasic._VERSION_.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+nuget.exe push "%rootPath%\DeveloperTools\ExcelDna.Testing\Package\nupkg\ExcelDna.Testing._VERSION_.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
+
+:end
