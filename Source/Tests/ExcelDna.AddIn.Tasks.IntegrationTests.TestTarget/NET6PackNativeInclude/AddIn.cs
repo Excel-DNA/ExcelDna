f4bd728afa4f0dde582c1622d80d413f961db8f1
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2023-01-07 18:25:32 +0500

    Created a test project.

---
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests.TestTarget/NET6PackNativeInclude/AddIn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using ExcelDna.Integration;
+
+namespace NET6PackNativeInclude
+{
+    public class AddIn : IExcelAddIn
+    {
+        [DllImport("MyNativeLibrary.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int MySum(int a1, int a2);
+
+        public void AutoOpen()
+        {
+            var thisAddInName = Path.GetFileName((string)XlCall.Excel(XlCall.xlGetName));
+            try
+            {
+                var message = string.Format("Excel-DNA Add-In '{0}' loaded!", thisAddInName);
+                message += Environment.NewLine + MySum(40, 2);
+                MessageBox.Show(message, thisAddInName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), thisAddInName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void AutoClose()
+        {
+        }
+    }
+}
