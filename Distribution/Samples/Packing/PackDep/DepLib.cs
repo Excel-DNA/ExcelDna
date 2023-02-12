16d99231d337079a3946bfad0b4aa8660939591e
Govert van Drimmelen <govert@dnakode.com>
2023-02-12 12:04:58 +0200

    Migrate Distribution/Samples to samples repository

--- Distribution/Samples/Packing/PackDep/DepLib.cs
+++
@@ -1,7 +0,0 @@
-public class SomeDepClass
-{
-    public static string OtherMessage()
-    {
-        return "Hello from The Other Lib (SomeDepClass.OtherMessage)";
-    }
-}
--- Distribution/Samples/Packing/PackRef/MakeMyLib.bat
+++
@@ -1,1 +0,0 @@
-c:\windows\microsoft.net\framework\v2.0.50727\csc.exe /reference:ExcelDna.Integration.dll /target:library MyLib.cs
