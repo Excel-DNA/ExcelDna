39886a240503b6245fdcb8e174479a7c99c89e8a
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2024-02-18 12:38:13 +0500

    Created TestTarget project.

---
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests.TestTarget/NET6CustomRuntimeConfiguration/AddIn.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Windows.Forms;
+using ExcelDna.Integration;
+
+namespace NET6CustomRuntimeConfiguration
+{
+    public class AddIn : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            var thisAddInName = Path.GetFileName((string)XlCall.Excel(XlCall.xlGetName));
+            var message = string.Format("Excel-DNA Add-In '{0}' loaded!", thisAddInName);
+
+            var webApplication = WebApplication.Create();
+            message += Environment.NewLine;
+            message += Environment.NewLine;
+            message += $"WebApplication Environment: {webApplication.Environment.EnvironmentName}.";
+
+            MessageBox.Show(message, thisAddInName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void AutoClose()
+        {
+        }
+    }
+}
