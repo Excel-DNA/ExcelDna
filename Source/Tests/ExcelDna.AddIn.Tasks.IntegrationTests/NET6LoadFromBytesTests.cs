3f88d2e0cb5f3c3bf81d35c45d0bd480f796d17a
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2024-08-11 13:26:44 +0500

    Added ExcelAddInLoadFromBytes project property.

---
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/NET6LoadFromBytesTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace ExcelDna.AddIn.Tasks.IntegrationTests
+{
+    [TestFixture]
+    public class NET6LoadFromBytesTests : IntegrationTestBase
+    {
+        [Test]
+        public void Default()
+        {
+            const string projectBasePath = @"NET6LoadFromBytes\";
+            const string projectOutDir = projectBasePath + @"bin\Release\";
+
+            Clean(projectOutDir);
+
+            MsBuild(projectBasePath + "NET6LoadFromBytes.csproj /t:Restore,Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"bin\Release\"));
+
+            AssertFileContains(Path.Combine(projectOutDir, "NET6LoadFromBytes-AddIn64.dna"), "LoadFromBytes=\"true\"");
+        }
+
+        [Test]
+        public void Disabled()
+        {
+            const string projectBasePath = @"NET6LoadFromBytes\";
+            const string projectOutDir = projectBasePath + @"bin\Release\";
+
+            Clean(projectOutDir);
+
+            MsBuild(projectBasePath + "NET6LoadFromBytes.csproj /t:Restore,Build /p:ExcelAddInLoadFromBytes=false /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"bin\Release\"));
+
+            AssertFileContains(Path.Combine(projectOutDir, "NET6LoadFromBytes-AddIn64.dna"), "LoadFromBytes=\"false\"");
+        }
+
+        [Test]
+        public void Enabled()
+        {
+            const string projectBasePath = @"NET6LoadFromBytes\";
+            const string projectOutDir = projectBasePath + @"bin\Release\";
+
+            Clean(projectOutDir);
+
+            MsBuild(projectBasePath + "NET6LoadFromBytes.csproj /t:Restore,Build /p:ExcelAddInLoadFromBytes=true /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"bin\Release\"));
+
+            AssertFileContains(Path.Combine(projectOutDir, "NET6LoadFromBytes-AddIn64.dna"), "LoadFromBytes=\"true\"");
+        }
+    }
+}
