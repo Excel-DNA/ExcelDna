cb27c803c6402a1dbbf8946ef34d8f2cbb914330
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2024-08-21 16:12:36 +0500

    Added a test in ExcelDnaUnpack mode.

--- Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/SDKPublishDocTests.cs
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests/SDKPublishDocTests.cs
@@ -21,5 +21,21 @@ namespace ExcelDna.AddIn.Tasks.IntegrationTests
 
             AssertNotFound(Path.Combine(publishDir, "SDKPublishDoc-AddIn.chm"));
         }
+
+        [Test]
+        public void UnPacked()
+        {
+            const string projectBasePath = @"SDKPublishDoc\";
+            const string projectOutDir = projectBasePath + @"bin\Release\";
+            const string publishDir = projectOutDir + @"publish\";
+
+            Clean(projectOutDir);
+
+            // The first build on a clean project doesn't create .chm at all, but the second run creates .chm and publishes it: 
+            for (int i = 0; i < 2; ++i)
+                MsBuild(projectBasePath + "SDKPublishDoc.csproj /p:ExcelDnaUnpack=true /t:Restore,Build /p:Configuration=Release /v:m " + MsBuildParam("OutputPath", @"bin\Release\"));
+
+            AssertOutput(publishDir, "*.chm", "SDKPublishDoc-AddIn.chm");
+        }
     }
 }
