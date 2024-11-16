4e79aecca7ff9816fd2108a9e2e6b9af50efc3e7
Govert van Drimmelen <govert@dnakode.com>
2024-11-16 15:31:26 +0200

    Update binaries

--- MasterBuild/PushAll.cmd
+++ MasterBuild/PushAll.cmd
@@ -5,37 +5,37 @@ set basePath=%currentPath:~0,-1%
 set rootPath=%~dp0..\..
 
 
-nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\ExcelDna.Integration.1.8.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\ExcelDna.Integration.1.9.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\ExcelDna.AddIn.1.8.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\ExcelDna.AddIn.1.9.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\Excel-DNA.Lib.1.8.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\Excel-DNA.Lib.1.9.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\Excel-DNA.1.8.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\Excel-DNA.1.9.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\ExcelDna.Templates.1.8.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%rootPath%\ExcelDna\Package\nupkg\ExcelDna.Templates.1.9.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%rootPath%\IntelliSense\NuGet\nupkg\ExcelDna.IntelliSense.1.8.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%rootPath%\IntelliSense\NuGet\nupkg\ExcelDna.IntelliSense.1.9.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%rootPath%\ExcelDnaDoc\Package\nupkg\ExcelDnaDoc.1.8.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%rootPath%\ExcelDnaDoc\Package\nupkg\ExcelDnaDoc.1.9.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%rootPath%\Registration\NuGet\nupkg\ExcelDna.Registration.1.8.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%rootPath%\Registration\NuGet\nupkg\ExcelDna.Registration.1.9.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%rootPath%\Registration\NuGet\nupkg\ExcelDna.Registration.FSharp.1.8.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%rootPath%\Registration\NuGet\nupkg\ExcelDna.Registration.FSharp.1.9.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%rootPath%\Registration\NuGet\nupkg\ExcelDna.Registration.VisualBasic.1.8.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%rootPath%\Registration\NuGet\nupkg\ExcelDna.Registration.VisualBasic.1.9.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%rootPath%\DeveloperTools\ExcelDna.Testing\Package\nupkg\ExcelDna.Testing.1.8.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%rootPath%\DeveloperTools\ExcelDna.Testing\Package\nupkg\ExcelDna.Testing.1.9.0-alpha3.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
 :end
