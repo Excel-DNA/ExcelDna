9d22447e60e6c2e3124e31f423f5d5b5c9c5e561
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2024-12-21 19:24:40 +0500

    Added AsyncTaskUtil.

---
+++ Source/ExcelDna.Integration/Registration/Utils/AsyncTaskUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+
+namespace ExcelDna.Registration.Utils
+{
+    // Helpers for creating and using Task-based functions with Excel-DNA's RTD-based IObservable support
+    public static class AsyncTaskUtil
+    {
+        public static object RunTask<TResult>(string callerFunctionName, object callerParameters, Func<Task<TResult>> taskSource)
+        {
+            return ExcelAsyncUtil.RunTask(callerFunctionName, callerParameters, taskSource);
+        }
+
+        // Careful - this might only work as long as the task is not shared between calls, since cancellation cancels that task
+        public static object RunTaskWithCancellation<TResult>(string callerFunctionName, object callerParameters, Func<CancellationToken, Task<TResult>> taskSource)
+        {
+            return ExcelAsyncUtil.RunTaskWithCancellation(callerFunctionName, callerParameters, taskSource);
+        }
+
+        public static object RunAsTask<TResult>(string callerFunctionName, object callerParameters, Func<TResult> function)
+        {
+            return ExcelAsyncUtil.RunAsTask(callerFunctionName, callerParameters, function);
+        }
+
+        public static object RunAsTaskWithCancellation<TResult>(string callerFunctionName, object callerParameters, Func<CancellationToken, TResult> function)
+        {
+            return ExcelAsyncUtil.RunAsTaskWithCancellation(callerFunctionName, callerParameters, function);
+        }
+    }
+}
