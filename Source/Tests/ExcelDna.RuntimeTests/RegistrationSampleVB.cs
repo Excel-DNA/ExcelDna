8f0eb514a904ce4392e3235f688150be5eed7625
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-01-05 19:26:09 +0500

    Added RangeParameterExamples and tests.

--- Source/Tests/ExcelDna.RuntimeTests/RegistrationSampleVB.cs
+++ Source/Tests/ExcelDna.RuntimeTests/RegistrationSampleVB.cs
@@ -35,6 +35,26 @@ namespace ExcelDna.RuntimeTests
                 Assert.Equal("pa-b", functionRange.Value.ToString());
             }
         }
+
+        [ExcelFact(Workbook = "", AddIn = @"..\..\..\..\ExcelDna.AddIn.RegistrationSampleVB\bin\Debug\net6.0-windows\ExcelDna.AddIn.RegistrationSampleVB-AddIn")]
+        public void Range()
+        {
+            {
+                Range functionRange = ((Worksheet)ExcelDna.Testing.Util.Workbook.Sheets[1]).Range["B1"];
+                functionRange.Formula = "=dnaVbRangeTest(B2)";
+                Assert.Equal("$B$2", functionRange.Value.ToString());
+            }
+            {
+                Range functionRange = ((Worksheet)ExcelDna.Testing.Util.Workbook.Sheets[1]).Range["C1"];
+                functionRange.Formula = "=dnaVbRangeTest(B2:C4)";
+                Assert.Equal("$B$2:$C$4", functionRange.Value.ToString());
+            }
+            {
+                Range functionRange = ((Worksheet)ExcelDna.Testing.Util.Workbook.Sheets[1]).Range["D1"];
+                functionRange.Formula = "=dnaVbRangeTest((B2,D5:E6))";
+                Assert.Equal("$B$2,$D$5:$E$6", functionRange.Value.ToString());
+            }
+        }
     }
 #endif
 }
