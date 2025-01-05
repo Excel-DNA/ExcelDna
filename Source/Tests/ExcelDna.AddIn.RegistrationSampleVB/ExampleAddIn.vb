32f8cde3978c9653b1a9d99be88f044c83440bad
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-01-05 10:41:19 +0500

    Created ExcelDna.AddIn.RegistrationSampleVB project.

---
+++ Source/Tests/ExcelDna.AddIn.RegistrationSampleVB/ExampleAddIn.vb
@@ -0,0 +1,18 @@
+﻿Imports ExcelDna.Integration
+
+Public Class ExampleAddIn
+    Implements IExcelAddIn
+
+    Public Sub AutoOpen() Implements IExcelAddIn.AutoOpen
+        ExcelIntegration.RegisterUnhandledExceptionHandler(Function(ex) "!!! ERROR: " + ex.ToString())
+
+        'PerformDefaultRegistration()
+
+        ' Could add Async too...
+    End Sub
+
+    Public Sub AutoClose() Implements IExcelAddIn.AutoClose
+
+    End Sub
+
+End Class
