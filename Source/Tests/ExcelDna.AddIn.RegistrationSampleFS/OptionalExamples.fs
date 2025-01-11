0797813da419081ef26921cf17ba2636abef6c0f
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-01-11 20:09:25 +0500

    Added F# optional example and tests.

---
+++ Source/Tests/ExcelDna.AddIn.RegistrationSampleFS/OptionalExamples.fs
@@ -0,0 +1,13 @@
+﻿namespace ExcelDna.AddIn.RegistrationSampleFS
+
+open ExcelDna.Integration
+
+type OptionalExamples =
+    [<ExcelFunction>]
+    static member dnaFSharpOptional(?value : double, ?str : string, ?bl : bool) = 
+        let theValue = defaultArg value 12.3
+        let theString = defaultArg str "qwerty"
+        let theBool = defaultArg bl true
+        sprintf "Value: %f, String: %s, Bool: %b" theValue theString theBool
+
+
