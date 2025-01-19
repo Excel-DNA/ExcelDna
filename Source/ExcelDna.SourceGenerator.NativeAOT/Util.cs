b33252ab5f1c53e4005fd49551532122c880d73c
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-01-19 15:04:02 +0500

    Added ExcelDna.SourceGenerator.NativeAOT project.

---
+++ Source/ExcelDna.SourceGenerator.NativeAOT/Util.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+
+namespace ExcelDna.SourceGenerator.NativeAOT
+{
+    public class Util
+    {
+        public static string GetFullTypeName(ITypeSymbol type)
+        {
+            return type.ToDisplayString();
+        }
+
+        public static string GetFullMethodName(IMethodSymbol method)
+        {
+            return $"{GetFullTypeName(method.ContainingType)}.{method.Name}";
+        }
+
+        public static IEnumerable<ITypeSymbol> GetBaseTypesAndThis(ITypeSymbol type)
+        {
+            var current = type;
+            while (current != null)
+            {
+                yield return current;
+                current = current.BaseType;
+            }
+        }
+    }
+}
