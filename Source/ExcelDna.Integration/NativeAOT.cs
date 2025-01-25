b253b114f9b47d2a96928dd76be828b0e67cad53
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-01-25 18:23:41 +0500

    Disabled AssemblyDependencyResolver when NativeAOT is active.

--- Source/ExcelDna.Integration/NativeAOT.cs
+++ Source/ExcelDna.Integration/NativeAOT.cs
@@ -5,6 +5,8 @@ namespace ExcelDna.Integration
 {
     public class NativeAOT
     {
+        public static bool IsActive { get; set; }
+
         public static List<MethodInfo> MethodsForRegistration { get; } = new List<MethodInfo>();
     }
 }
