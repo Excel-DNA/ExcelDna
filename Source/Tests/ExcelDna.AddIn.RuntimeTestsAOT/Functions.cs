074bdbe14d0f09cf47910a463574c42a5a1ac5be
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-01-26 10:23:38 +0500

    Added ExcelDna.AddIn.RuntimeTestsAOT project and tests.

---
+++ Source/Tests/ExcelDna.AddIn.RuntimeTestsAOT/Functions.cs
@@ -0,0 +1,19 @@
+﻿using ExcelDna.Integration;
+
+namespace ExcelDna.AddIn.RuntimeTestsAOT
+{
+    public class Functions
+    {
+        [ExcelFunction]
+        public static string NativeHello(string name)
+        {
+            return $"Hello {name}!";
+        }
+
+        [ExcelFunction]
+        public static int NativeSum(int i1, int i2)
+        {
+            return i1 + i2;
+        }
+    }
+}
