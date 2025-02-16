d0191e2943a7912a117e54e453dff6327baa7cef
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-02-16 13:50:33 +0500

    Implemented loading of IExcelAddIn(s).

--- Source/ExcelDna.Integration/AssemblyLoader.cs
+++ Source/ExcelDna.Integration/AssemblyLoader.cs
@@ -89,7 +89,7 @@ namespace ExcelDna.Integration
                             GetExcelMethods(type, explicitExports, methods, excelFunctionsExtendedRegistration);
                             GetExcelFunctionExecutionHandlerSelectors(type, excelFunctionExecutionHandlerSelectors);
                         }
-                        GetExcelAddIns(assembly, type, loadRibbons, addIns);
+                        GetExcelAddIns(assembly, new TypeHelperDynamic(type), loadRibbons, addIns);
                         GetRtdServerTypes(type, rtdServerTypes, out isRtdServer);
                         GetComClassTypes(assembly, type, attribs, isRtdServer, comClassTypes);
                     }
@@ -292,38 +292,38 @@ namespace ExcelDna.Integration
             public DnaLibrary ParentDnaLibrary;
         }
 
-        static public void GetExcelAddIns(ExportedAssembly assembly, Type t, bool loadRibbons, List<ExcelAddInInfo> addIns)
+        static public void GetExcelAddIns(ExportedAssembly assembly, ITypeHelper t, bool loadRibbons, List<ExcelAddInInfo> addIns)
         {
             // NOTE: We probably should have restricted this to public types, but didn't. Now it's too late.
             //       So internal classes that implement IExcelAddIn are also loaded.
             try
             {
-                if (!t.IsClass || t.IsAbstract)
+                if (!t.Type.IsClass || t.Type.IsAbstract)
                 {
-                    Logger.Registration.Verbose("GetExcelAddIns - Skipped add-in object of type: {0}", t.FullName);
+                    Logger.Registration.Verbose("GetExcelAddIns - Skipped add-in object of type: {0}", t.Type.FullName);
                     return;
                 }
 
-                Type addInType = t.GetInterface("ExcelDna.Integration.IExcelAddIn");
-                bool isRibbon = IsRibbonType(t);
+                Type addInType = t.Type.GetInterface("ExcelDna.Integration.IExcelAddIn");
+                bool isRibbon = IsRibbonType(t.Type);
                 if (addInType != null || (isRibbon && loadRibbons))
                 {
                     ExcelAddInInfo info = new ExcelAddInInfo();
                     if (addInType != null)
                     {
-                        info.AutoOpenMethod = addInType.GetMethod("AutoOpen");
-                        info.AutoCloseMethod = addInType.GetMethod("AutoClose");
+                        info.AutoOpenMethod = typeof(IExcelAddIn).GetMethod("AutoOpen");
+                        info.AutoCloseMethod = typeof(IExcelAddIn).GetMethod("AutoClose");
                     }
                     info.IsCustomUI = isRibbon;
-                    info.Instance = Activator.CreateInstance(t);
-                    info.ParentDnaLibrary = assembly.ParentDnaLibrary;
+                    info.Instance = t.CreateInstance();
+                    info.ParentDnaLibrary = assembly?.ParentDnaLibrary;
                     addIns.Add(info);
-                    Logger.Registration.Verbose("GetExcelAddIns - Created add-in object of type: {0}", t.FullName);
+                    Logger.Registration.Verbose("GetExcelAddIns - Created add-in object of type: {0}", t.Type.FullName);
                 }
             }
             catch (Exception e) // I think only CreateInstance can throw an exception here...
             {
-                Logger.Initialization.Warn("GetExcelAddIns CreateInstance problem for type: {0} - exception: {1}", t.FullName, e);
+                Logger.Initialization.Warn("GetExcelAddIns CreateInstance problem for type: {0} - exception: {1}", t.Type.FullName, e);
             }
 
         }
