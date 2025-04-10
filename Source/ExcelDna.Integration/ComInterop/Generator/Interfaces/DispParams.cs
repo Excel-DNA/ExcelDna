cf1152f5ccb4d8baff57810522fded86b388b0c3
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-04-10 23:06:21 +0500

    Copied Generator.Interfaces to ExcelDna.Integration project.

---
+++ Source/ExcelDna.Integration/ComInterop/Generator/Interfaces/DispParams.cs
@@ -0,0 +1,14 @@
+﻿#if COM_GENERATED
+
+namespace ExcelDna.Integration.ComInterop.Generator.Interfaces
+{
+    internal struct DispParams
+    {
+        public int cArgs;
+        public int cNamedArgs;
+        public int rgdispidNamedArgs;
+        public Variant[] rgvarg;
+    }
+}
+
+#endif
