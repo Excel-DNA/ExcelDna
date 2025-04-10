692e8a4065a107baf47944efdf7dc1798e388f73
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-04-10 23:06:21 +0500

    Copied Generator.Interfaces to ExcelDna.Integration project.

---
+++ Source/ExcelDna.Integration/ComInterop/Generator/Interfaces/DispParamsMarshaller.cs
@@ -0,0 +1,42 @@
+﻿#if COM_GENERATED
+
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace ExcelDna.Integration.ComInterop.Generator.Interfaces
+{
+    [CustomMarshaller(typeof(DispParams), MarshalMode.Default, typeof(DispParamsMarshaller))]
+    internal static class DispParamsMarshaller
+    {
+        public static unsafe DispParamsNative ConvertToUnmanaged(DispParams managed)
+        {
+            return new DispParamsNative
+            {
+                cArgs = managed.cArgs,
+                cNamedArgs = managed.cNamedArgs,
+                rgdispidNamedArgs = &managed.rgdispidNamedArgs,
+                rgvarg =
+                    managed.rgvarg != null
+                        ? ArrayMarshaller.ArrayToPtr(managed.rgvarg.Reverse().Select(VariantMarshaller.ConvertToUnmanaged).ToArray())
+                        : nint.Zero
+            };
+        }
+
+        public static unsafe DispParams ConvertToManaged(DispParamsNative unmanaged)
+        {
+            return new DispParams
+            {
+                cArgs = unmanaged.cArgs,
+                cNamedArgs = unmanaged.cNamedArgs,
+                rgdispidNamedArgs = *unmanaged.rgdispidNamedArgs,
+                rgvarg = ArrayMarshaller
+                    .PtrToArray<VariantNative>(unmanaged.rgvarg, unmanaged.cArgs)
+                    .Select(VariantMarshaller.ConvertToManaged)
+                    .Reverse()
+                    .ToArray(),
+            };
+        }
+    }
+}
+
+#endif
