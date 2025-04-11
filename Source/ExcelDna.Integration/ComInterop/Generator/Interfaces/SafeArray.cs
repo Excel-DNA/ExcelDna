b84986ce10d15fdd51440a4d0c200d58d929707b
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-04-11 22:30:14 +0500

    Created source generated IDTExtensibility2 interface.

---
+++ Source/ExcelDna.Integration/ComInterop/Generator/Interfaces/SafeArray.cs
@@ -0,0 +1,32 @@
+﻿#if COM_GENERATED
+
+using System.Runtime.CompilerServices;
+
+namespace ExcelDna.Integration.ComInterop.Generator.Interfaces
+{
+    internal partial struct SAFEARRAYBOUND
+    {
+        public uint cElements;
+        public int lLbound;
+    }
+
+    [InlineArray(InlineArraySAFEARRAYBOUND_1.Length)]
+    internal partial struct InlineArraySAFEARRAYBOUND_1
+    {
+        public const int Length = 1;
+
+        public SAFEARRAYBOUND Data;
+    }
+
+    internal partial struct SafeArray
+    {
+        public ushort cDims;
+        public ushort fFeatures;
+        public uint cbElements;
+        public uint cLocks;
+        public nint pvData;
+        public InlineArraySAFEARRAYBOUND_1 rgsabound; // variable-length array placeholder
+    }
+}
+
+#endif
