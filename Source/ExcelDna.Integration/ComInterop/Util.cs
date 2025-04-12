5a829d0947f635d9d790a01347ba0662578a43db
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-04-12 14:38:55 +0500

    Implemented COM wrapper type detection for an object.

--- Source/ExcelDna.Integration/ComInterop/Util.cs
+++ Source/ExcelDna.Integration/ComInterop/Util.cs
@@ -1,10 +1,16 @@
-﻿namespace ExcelDna.Integration.ComInterop
+﻿using System;
+using System.Runtime.InteropServices;
+#if COM_GENERATED
+using System.Runtime.InteropServices.Marshalling;
+#endif
+
+namespace ExcelDna.Integration.ComInterop
 {
     internal static class Util
     {
         private static TypeAdapter typeAdapter = new TypeAdapter();
-
 #if COM_GENERATED
+        private static ComWrappers comWrappers = new StrategyBasedComWrappers();
         private static Generator.TypeAdapter generatorAdapter = new();
 #endif
 
@@ -17,6 +23,27 @@
 #else
                 return typeAdapter;
 #endif
+            }
+        }
+
+        public static int QueryInterfaceForObject(object o, Guid guid, out IntPtr ppv)
+        {
+            Guid iid = guid;
+            return Marshal.QueryInterface(GetIUnknownForObject(o), ref iid, out ppv);
+        }
+
+        private static IntPtr GetIUnknownForObject(object o)
+        {
+#if COM_GENERATED
+            Type t = o.GetType();
+            if (t == typeof(ComObject) || t.GetCustomAttributes(typeof(GeneratedComClassAttribute), false).Length > 0)
+            {
+                return comWrappers.GetOrCreateComInterfaceForObject(o, CreateComInterfaceFlags.None);
+            }
+            else
+#endif
+            {
+                return Marshal.GetIUnknownForObject(o);
             }
         }
     }
