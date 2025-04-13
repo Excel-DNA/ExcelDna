a57f1728d1b5d08f0dd58efe39a49a88b6281335
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-04-13 17:29:06 +0500

    Limited IExcelRibbon to .NET 8.

--- Source/ExcelDna.Integration/IExcelRibbon.cs
+++ Source/ExcelDna.Integration/IExcelRibbon.cs
@@ -1,7 +1,11 @@
-﻿namespace ExcelDna.Integration.CustomUI
+﻿#if COM_GENERATED
+
+namespace ExcelDna.Integration.CustomUI
 {
     public interface IExcelRibbon
     {
         string GetCustomUI(string RibbonID);
     }
 }
+
+#endif
