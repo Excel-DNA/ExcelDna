2e64da612e11f83f1322ba007d6da29faaa79079
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-05-03 14:33:36 +0500

    Added RibbonControl Tag support.

--- Source/Tests/ExcelDna.AddIn.RuntimeTestsAOT/RibbonController.cs
+++ Source/Tests/ExcelDna.AddIn.RuntimeTestsAOT/RibbonController.cs
@@ -12,8 +12,8 @@ namespace ExcelDna.AddIn.RuntimeTestsAOT
         <tabs>
           <tab id='tab1' label='My Native Tab'>
             <group id='group1' label='My Native Group'>
-              <button id='button1' label='My Native Button1' onAction='OnButtonPressed1'/>
-              <button id='button2' label='My Native Button2' onAction='OnButtonPressed2'/>
+              <button id='button1' tag='tagdata1' label='My Native Button1' onAction='OnButtonPressed1'/>
+              <button id='button2' tag='tagdata2' label='My Native Button2' onAction='OnButtonPressed2'/>
             </group >
           </tab>
         </tabs>
@@ -23,12 +23,12 @@ namespace ExcelDna.AddIn.RuntimeTestsAOT
 
         public void OnButtonPressed1(RibbonControl control)
         {
-            MessageBox.Show("Hello1 from native control " + control.Id);
+            MessageBox.Show($"Hello1 from native control id={control.Id} tag={control.Tag}");
         }
 
         public void OnButtonPressed2(RibbonControl control)
         {
-            MessageBox.Show("Hello2 from native control " + control.Id);
+            MessageBox.Show($"Hello2 from native control id={control.Id} tag={control.Tag}");
         }
     }
 }
