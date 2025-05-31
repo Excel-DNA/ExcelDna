1d89ea78d61c7c0ba3176c109783f62cd6f60d05
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-05-31 13:49:26 +0500

    Added automated runtime tests for a native async function.

---
+++ Source/Tests/ExcelDna.RuntimeTests/NativeAOTOutOfProcess.cs
@@ -0,0 +1,25 @@
+﻿using ExcelDna.Testing;
+using Microsoft.Office.Interop.Excel;
+using Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace ExcelDna.RuntimeTests
+{
+    [ExcelTestSettings(OutOfProcess = true)]
+    [Collection("OutOfProcess")]
+    public class NativeAOTOutOfProcess
+    {
+        [ExcelFact(Workbook = "", AddIn = AddInPath.RuntimeTestsAOT)]
+        public void AsyncTask()
+        {
+            {
+                Range functionRange = ((Worksheet)ExcelDna.Testing.Util.Workbook.Sheets[1]).Range["B1"];
+                functionRange.Formula = "=NativeAsyncTaskHello(\"world\", 200)";
+
+                Automation.WaitFor(() => functionRange.Value?.ToString() == "Hello native async task world", 1000);
+
+                Assert.Equal("Hello native async task world", functionRange.Value.ToString());
+            }
+        }
+    }
+}
+
