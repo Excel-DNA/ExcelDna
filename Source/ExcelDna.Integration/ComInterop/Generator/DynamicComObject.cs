e4861a30625a2b02531e634392d53cd47e38a6fd
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-06-08 17:34:51 +0500

    Implemented SetProperty, indexing, Invoke, dynamic return for DynamicComObject.

--- Source/ExcelDna.Integration/ComInterop/Generator/DynamicComObject.cs
+++ Source/ExcelDna.Integration/ComInterop/Generator/DynamicComObject.cs
@@ -15,7 +15,46 @@ namespace ExcelDna.Integration.ComInterop.Generator
 
         public object? GetProperty(string name)
         {
-            return dispatchObject.GetProperty(name);
+            return WrapDispatch(dispatchObject.GetProperty(name));
+        }
+
+        public object? GetProperty(string name, object[]? args)
+        {
+            return WrapDispatch(dispatchObject.GetProperty(name, args));
+        }
+
+        public void SetProperty(string name, object value)
+        {
+            dispatchObject.SetProperty(name, value);
+        }
+
+        public object? this[int index]
+        {
+            get
+            {
+                return WrapDispatch(dispatchObject.GetIndex(index));
+            }
+        }
+
+        public object? this[string index]
+        {
+            get
+            {
+                return WrapDispatch(dispatchObject.GetIndex(index));
+            }
+        }
+
+        public object? Invoke(string name, object[] args)
+        {
+            return WrapDispatch(dispatchObject.Invoke(name, args));
+        }
+
+        private static object? WrapDispatch(object? o)
+        {
+            if (o is Interfaces.DispatchObject dispatch)
+                return new DynamicComObject(dispatch);
+
+            return o;
         }
     }
 }
