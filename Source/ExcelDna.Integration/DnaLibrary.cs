b0940c765962a3ebd38c7ab5de26f19accd1c8a3
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-08-17 14:36:52 +0500

    Fixed command menu registration.

--- Source/ExcelDna.Integration/DnaLibrary.cs
+++ Source/ExcelDna.Integration/DnaLibrary.cs
@@ -329,8 +329,11 @@ namespace ExcelDna.Integration
             AssemblyLoader.GetExcelMethods(NativeAOT.MethodsForRegistration, true, _methods, _excelFunctionsExtendedRegistration);
 
             // Register my Methods
-            var allMethods = _methods.Select(i => new Registration.ExcelFunctionRegistration(i)).Concat(_excelFunctionsExtendedRegistration);
-            ExtendedRegistration.Registration.RegisterExtended(allMethods, _excelParameterConversions, _excelReturnConversions, _excelFunctionProcessors, _excelFunctionExecutionHandlerSelectors);
+            List<MethodInfo> commands = _methods.Where(Registration.ExcelCommandRegistration.IsCommand).ToList();
+            ExcelIntegration.RegisterMethods(commands);
+
+            var functions = _methods.Except(commands).Select(i => new Registration.ExcelFunctionRegistration(i)).Concat(_excelFunctionsExtendedRegistration);
+            ExtendedRegistration.Registration.RegisterExtended(functions, _excelParameterConversions, _excelReturnConversions, _excelFunctionProcessors, _excelFunctionExecutionHandlerSelectors);
 
             // Invoke AutoOpen in all assemblies
             foreach (AssemblyLoader.ExcelAddInInfo addIn in _addIns)
---
+++ Source/Tests/ExcelDna.RuntimeTests/UI.cs
@@ -0,0 +1,53 @@
+﻿using ExcelDna.Integration;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Excel;
+using Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace ExcelDna.RuntimeTests
+{
+    public class UI
+    {
+        [ExcelFact(Workbook = "", AddIn = AddInPath.RuntimeTests)]
+        public void Command()
+        {
+            CommandBarPopup? menu = FindPopupMenu("ExcelDna.AddIn.RuntimeTests add-in");
+            Assert.NotNull(menu);
+
+            CommandBarButton? button = FindButton(menu, "MyCommandHello");
+            Assert.NotNull(button);
+
+            button.Execute();
+
+            Range functionRange = ((Worksheet)ExcelDna.Testing.Util.Workbook.Sheets[1]).Range["B1"];
+            functionRange.Formula = "=MyFunctionExecutionLog()";
+            Assert.True(functionRange.Value.ToString().Contains("Hello command."));
+        }
+
+        private static CommandBarPopup? FindPopupMenu(string name)
+        {
+            var app = (Microsoft.Office.Interop.Excel.Application)ExcelDnaUtil.Application;
+            var controls = app.CommandBars[1].Controls;
+            for (int i = 1; i <= controls.Count; i++)
+            {
+                CommandBarControl control = controls[i];
+                if (control.Caption == name && control is CommandBarPopup menu)
+                    return menu;
+            }
+
+            return null;
+        }
+
+        private static CommandBarButton? FindButton(CommandBarPopup menu, string name)
+        {
+            CommandBarControls menuControls = menu.Controls;
+            for (int i = 1; i <= menuControls.Count; i++)
+            {
+                CommandBarControl control = menuControls[i];
+                if (control.Caption == name && control is CommandBarButton button)
+                    return button;
+            }
+
+            return null;
+        }
+    }
+}
