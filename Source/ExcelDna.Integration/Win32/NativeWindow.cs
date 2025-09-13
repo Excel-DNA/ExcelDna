67b2d318fd6124092a0b9b53d7e3a45d14e272f0
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-09-13 20:39:46 +0500

    Added Win32 definitions.

---
+++ Source/ExcelDna.Integration/Win32/NativeWindow.cs
@@ -0,0 +1,26 @@
+﻿#if !USE_WINDOWS_FORMS
+
+namespace ExcelDna.Integration.Win32
+{
+    internal class NativeWindow
+    {
+        public System.IntPtr Handle { get; }
+
+        public virtual void CreateHandle(CreateParams cp)
+        {
+
+        }
+
+        public virtual void DestroyHandle()
+        {
+
+        }
+
+        protected virtual void WndProc(ref Message m)
+        {
+
+        }
+    }
+}
+
+#endif
