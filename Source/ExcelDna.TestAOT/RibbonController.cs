1433491b8da9ffe4d4e82f6169c245726e4ba5f7
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-10-05 15:20:02 +0500

    Implemented support for custom ribbon images using Win32 API.

--- Source/ExcelDna.TestAOT/RibbonController.cs
+++ Source/ExcelDna.TestAOT/RibbonController.cs
@@ -7,13 +7,13 @@ namespace ExcelDna.TestAOT
         public string GetCustomUI(string RibbonID)
         {
             return @"
-        <customUI xmlns='http://schemas.microsoft.com/office/2006/01/customui'>
+        <customUI xmlns='http://schemas.microsoft.com/office/2006/01/customui' loadImage='LoadImage'>
             <ribbon>
                 <tabs>
                     <tab id='tab1' label='My Native Tab'>
                     <group id='group1' label='My Native Group'>
-                        <button id='button1' tag='tagdata1' label='My Native Button1' onAction='OnButtonPressed1'/>
-                        <button id='button2' tag='tagdata2' label='My Native Button2' onAction='OnButtonPressed2'/>
+                        <button id='button1' tag='tagdata1' label='My Native Button1' image='ExcelDna.TestAOT.app.ico' onAction='OnButtonPressed1'/>
+                        <button id='button2' tag='tagdata2' label='My Native Button2' image='ExcelDna.TestAOT.Package.ico' onAction='OnButtonPressed2'/>
                     </group >
                     </tab>
                 </tabs>
