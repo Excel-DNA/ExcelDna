5adca8416d97fff27c6ae25cd5e76fff3ba43d7e
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-11-16 13:56:41 +0500

    Added a native library with pdb to the test project.

--- Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests.TestTarget/AOTPackPdb/AddIn.cs
+++ Source/Tests/ExcelDna.AddIn.Tasks.IntegrationTests.TestTarget/AOTPackPdb/AddIn.cs
@@ -10,10 +10,14 @@ namespace AOTPackPdb
         [DllImport("kernel32.dll")]
         static extern void DebugBreak();
 
+        [DllImport("MyNativeLibrary.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int MySum(int a1, int a2);
+
         public void AutoOpen()
         {
             var thisAddInName = Path.GetFileName((string)XlCall.Excel(XlCall.xlGetName));
             var message = string.Format("Excel-DNA Add-In '{0}' loaded!", thisAddInName);
+            message += Environment.NewLine + MySum(40, 2);
 
             DebugBreak();
         }
