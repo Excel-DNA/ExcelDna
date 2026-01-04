310add2caccaf0af86fe66cf016f344b9aac7942
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2026-01-04 14:20:37 +0500

    Added support for ExcelAsyncUtil.QueueMacro().

---
+++ Source/ExcelDna.TestAOT/Commands.cs
@@ -0,0 +1,19 @@
+﻿using ExcelDna.Integration;
+
+namespace ExcelDna.TestAOT
+{
+    public class Commands
+    {
+        [ExcelCommand(MenuText = "NativeTestCommand")]
+        public static void NativeTestCommand()
+        {
+            System.Diagnostics.Trace.WriteLine("ExcelDna.Test NativeTestCommand");
+        }
+
+        [ExcelCommand(MenuText = "NativeQueueMacro")]
+        public static void NativeQueueMacro()
+        {
+            ExcelAsyncUtil.QueueMacro("NativeTestCommand");
+        }
+    }
+}
