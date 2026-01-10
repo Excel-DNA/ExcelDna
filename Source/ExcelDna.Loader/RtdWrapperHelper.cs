b61a3267077f82513fb5e0c86f9eb107b0952ca3
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2026-01-10 20:33:14 +0500

    Replaced Marshal.SizeOf() with Marshal.SizeOf<>.

--- Source/ExcelDna.Loader/RtdWrapperHelper.cs
+++ Source/ExcelDna.Loader/RtdWrapperHelper.cs
@@ -20,12 +20,12 @@ namespace ExcelDna.Loader
         readonly XlOper12* _progIdXloper12;
         readonly XlOper12* _emptyStringXloper12;
         readonly XlOper12* _errorValueXloper12;
-        
+
         const int MaxCallParams = 22;
         // TODO: Sort out ThreadLocal to make this thread-safe when we support thread-safe RTD
         readonly XlOper12* _resultXloper12;  // Not Thread-Safe !?
         readonly XlOper12** _callParams;         // Array with MaxCallParams Xloper12s
-//        readonly XlString12** _callParamStrings; // Array with MaxCallParams XlString12s Only used for spill-over strings from long parameters
+                                                 //        readonly XlString12** _callParamStrings; // Array with MaxCallParams XlString12s Only used for spill-over strings from long parameters
 
         public RtdWrapperHelper(string progId, object rtdWrapperOptions)
         {
@@ -35,13 +35,13 @@ namespace ExcelDna.Loader
             // TODO: Can't easily use the regular marshallers, because we want to opt out of the memory management
             // TODO: But we should refactor this conversion code a bit
 
-            int size = Marshal.SizeOf(typeof(XlOper12));
-            int stringSize = Marshal.SizeOf(typeof(XlString12)) + ((XlString12.MaxLength - 1) /* 1 char is in Data[1] */ * 2 /* 2 bytes per char */);
-            
+            int size = Marshal.SizeOf<XlOper12>();
+            int stringSize = Marshal.SizeOf<XlString12>() + ((XlString12.MaxLength - 1) /* 1 char is in Data[1] */ * 2 /* 2 bytes per char */);
+
             _progIdXloper12 = (XlOper12*)Marshal.AllocCoTaskMem(size);
             _progIdXloper12->xlType = XlType12.XlTypeString;
             _progIdXloper12->pstrValue = (XlString12*)Marshal.AllocCoTaskMem(stringSize);
-            XlString12 * pdest = _progIdXloper12->pstrValue;
+            XlString12* pdest = _progIdXloper12->pstrValue;
             ushort charCount = (ushort)Math.Min(progId.Length, XlString12.MaxLength);
             fixed (char* psrc = progId)
             {
@@ -56,7 +56,7 @@ namespace ExcelDna.Loader
 
             _emptyStringXloper12 = (XlOper12*)Marshal.AllocCoTaskMem(size);
             _emptyStringXloper12->xlType = XlType12.XlTypeString;
-            _emptyStringXloper12->pstrValue = (XlString12*)Marshal.AllocCoTaskMem(Marshal.SizeOf(typeof(XlString12)));
+            _emptyStringXloper12->pstrValue = (XlString12*)Marshal.AllocCoTaskMem(Marshal.SizeOf<XlString12>());
             _emptyStringXloper12->pstrValue->Length = 0;
 
             _errorValueXloper12 = (XlOper12*)Marshal.AllocCoTaskMem(size);
@@ -70,7 +70,7 @@ namespace ExcelDna.Loader
             _callParams[0] = _progIdXloper12;
             _callParams[1] = _emptyStringXloper12;
 
-//            _callParamStrings = (XlString12**)Marshal.AllocCoTaskMem(stringSize * MaxCallParams);
+            //            _callParamStrings = (XlString12**)Marshal.AllocCoTaskMem(stringSize * MaxCallParams);
         }
 
         // This is the function we register with Excel
