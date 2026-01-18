58b86a92e46f0582f17c667fed62f27c191d1d0a
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2026-01-18 18:19:32 +0500

    Added extended delegate type to lambda construction.

--- Source/ExcelDna.Integration/Registration/ExcelFunctionRegistration.cs
+++ Source/ExcelDna.Integration/Registration/ExcelFunctionRegistration.cs
@@ -108,7 +108,9 @@ namespace ExcelDna.Registration
             var paramExprs = methodInfo.GetParameters()
                              .Select(pi => Expression.Parameter(pi.ParameterType, pi.Name))
                              .ToList();
-            FunctionLambda = Expression.Lambda(Expression.Call(methodInfo, paramExprs), methodInfo.Name, paramExprs);
+            FunctionLambda = (paramExprs.Count > 16) ?
+                Expression.Lambda(GetExtendedDelegateType(methodInfo), Expression.Call(methodInfo, paramExprs), methodInfo.Name, paramExprs) :
+                Expression.Lambda(Expression.Call(methodInfo, paramExprs), methodInfo.Name, paramExprs);
 
             var allMethodAttributes = methodInfo.GetCustomAttributes(true);
             foreach (var att in allMethodAttributes)
@@ -143,6 +145,28 @@ namespace ExcelDna.Registration
 
             // Check that we haven't made a mistake
             Debug.Assert(IsValid());
+        }
+
+        private static Type GetExtendedDelegateType(MethodInfo methodInfo)
+        {
+            if (methodInfo.ReturnType != typeof(void))
+            {
+                Type genericBase = ExtendedFuncUtil.GetFuncType(methodInfo.GetParameters().Length);
+                var args = methodInfo.GetParameters().Select(p => p.ParameterType)
+                    .Concat(new[] { methodInfo.ReturnType })
+                    .ToArray();
+                return genericBase.MakeGenericType(args);
+            }
+            else
+            {
+                Type genericBase = ExtendedFuncUtil.GetActionType(methodInfo.GetParameters().Length);
+                if (!genericBase.IsGenericType)
+                    return genericBase;
+
+                var args = methodInfo.GetParameters().Select(p => p.ParameterType)
+                    .ToArray();
+                return genericBase.MakeGenericType(args);
+            }
         }
     }
 }
