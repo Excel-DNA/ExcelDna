88ded817d326096107981fa1b4a5fec7679e461a
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2026-01-22 19:37:17 +0500

    Added .NET 4 runtime tests.

--- Source/Tests/ExcelDna.RuntimeTests/AddInPath.cs
+++ Source/Tests/ExcelDna.RuntimeTests/AddInPath.cs
@@ -5,12 +5,14 @@
 #if DEBUG
         public const string RuntimeTestsAOT = @"..\..\..\..\ExcelDna.AddIn.RuntimeTestsAOT\bin\Debug\net10.0-windows\win-x64\publish\ExcelDna.AddIn.RuntimeTestsAOT";
         public const string RuntimeTests = @"..\..\..\..\ExcelDna.AddIn.RuntimeTests\bin\Debug\net6.0-windows\ExcelDna.AddIn.RuntimeTests-AddIn";
+        public const string RuntimeTestsNET4 = @"..\..\..\..\ExcelDna.AddIn.RuntimeTestsNET4\bin\Debug\net48\ExcelDna.AddIn.RuntimeTestsNET4-AddIn";
         public const string RegistrationSample = @"..\..\..\..\ExcelDna.AddIn.RegistrationSample\bin\Debug\net6.0-windows\ExcelDna.AddIn.RegistrationSample-AddIn";
         public const string RegistrationSampleFS = @"..\..\..\..\ExcelDna.AddIn.RegistrationSampleFS\bin\Debug\net6.0-windows\ExcelDna.AddIn.RegistrationSampleFS-AddIn";
         public const string RegistrationSampleVB = @"..\..\..\..\ExcelDna.AddIn.RegistrationSampleVB\bin\Debug\net6.0-windows\ExcelDna.AddIn.RegistrationSampleVB-AddIn";
 #else
         public const string RuntimeTestsAOT = @"..\..\..\..\ExcelDna.AddIn.RuntimeTestsAOT\bin\Release\net10.0-windows\win-x64\publish\ExcelDna.AddIn.RuntimeTestsAOT";
         public const string RuntimeTests = @"..\..\..\..\ExcelDna.AddIn.RuntimeTests\bin\Release\net6.0-windows\ExcelDna.AddIn.RuntimeTests-AddIn";
+        public const string RuntimeTestsNET4 = @"..\..\..\..\ExcelDna.AddIn.RuntimeTestsNET4\bin\Release\net48\ExcelDna.AddIn.RuntimeTestsNET4-AddIn";
         public const string RegistrationSample = @"..\..\..\..\ExcelDna.AddIn.RegistrationSample\bin\Release\net6.0-windows\ExcelDna.AddIn.RegistrationSample-AddIn";
         public const string RegistrationSampleFS = @"..\..\..\..\ExcelDna.AddIn.RegistrationSampleFS\bin\Release\net6.0-windows\ExcelDna.AddIn.RegistrationSampleFS-AddIn";
         public const string RegistrationSampleVB = @"..\..\..\..\ExcelDna.AddIn.RegistrationSampleVB\bin\Release\net6.0-windows\ExcelDna.AddIn.RegistrationSampleVB-AddIn";
---
+++ Source/Tests/ExcelDna.RuntimeTests/NET4.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Office.Interop.Excel;
+using Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace ExcelDna.RuntimeTests
+{
+    public class NET4
+    {
+        [ExcelFact(Workbook = "", AddIn = AddInPath.RuntimeTestsNET4)]
+        public void OptionalDateTime()
+        {
+            Range functionRange = ((Worksheet)ExcelDna.Testing.Util.Workbook.Sheets[1]).Range["B1:B1"];
+            functionRange.Formula = "=Net4OptionalDateTime(\"2024/11/21\")";
+            Assert.Equal(".NET 4 Optional DateTime: 11/21/2024 12:00:00 AM", functionRange.Value.ToString());
+
+            functionRange.Formula = "=Net4OptionalDateTime()";
+            Assert.Equal(".NET 4 Optional DateTime: 1/1/0001 12:00:00 AM", functionRange.Value.ToString());
+        }
+    }
+}
