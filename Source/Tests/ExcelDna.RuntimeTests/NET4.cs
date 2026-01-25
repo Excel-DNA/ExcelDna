bc2c49af5242f5ea43cb74d5027ac7a84081ec50
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2026-01-25 18:29:09 +0500

    Added .NET 4 default DateTime runtime tests.

--- Source/Tests/ExcelDna.RuntimeTests/NET4.cs
+++ Source/Tests/ExcelDna.RuntimeTests/NET4.cs
@@ -15,5 +15,16 @@ namespace ExcelDna.RuntimeTests
             functionRange.Formula = "=Net4OptionalDateTime()";
             Assert.Equal(".NET 4 Optional DateTime: 1/1/0001 12:00:00 AM", functionRange.Value.ToString());
         }
+
+        [ExcelFact(Workbook = "", AddIn = AddInPath.RuntimeTestsNET4)]
+        public void DefaultDateTime()
+        {
+            Range functionRange = ((Worksheet)ExcelDna.Testing.Util.Workbook.Sheets[1]).Range["B1"];
+            functionRange.Formula = "=Net4DateTimeDefault()";
+            Assert.Equal<double>(0, functionRange.Value2);
+
+            functionRange.Formula = "=Net4DateTimeDefault(\"2025-10-13\")";
+            Assert.Equal<double>(45943, functionRange.Value2);
+        }
     }
 }
