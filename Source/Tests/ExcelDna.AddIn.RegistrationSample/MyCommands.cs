260313f312f90d4691e3dd9141a814628e04a1bd
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2026-03-07 21:59:53 +0500

    Excluded generic methods from manual registration.

---
+++ Source/Tests/ExcelDna.AddIn.RegistrationSample/MyCommands.cs
@@ -0,0 +1,18 @@
+﻿using ExcelDna.Integration;
+
+namespace ExcelDna.AddIn.RegistrationSample
+{
+    public static class MyCommands
+    {
+        [ExcelCommand(MenuName = "My Commands", MenuText = "Command")]
+        public static void MyCommand()
+        {
+            return;
+        }
+
+        public static void MyGenericCommand<T>()
+        {
+            return;
+        }
+    }
+}
